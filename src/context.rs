a8c4e9d3be0d73b030cd951e654cd3b41fdcfdc7
Jason Asher <swo9@cdc.gov>
2024-08-04 12:12:19 -0400

    Add Context (#4)

---
+++ src/context.rs
@@ -0,0 +1,355 @@
+//! A manager for the state of a discrete-event simulation
+//!
+//! Defines a `Context` that is intended to provide the foundational mechanism
+//! for storing and manipulating the state of a given simulation.
+use std::{
+    any::{Any, TypeId},
+    collections::{HashMap, VecDeque},
+};
+
+use crate::plan::{Id, Queue};
+
+/// The common callback used by multiple `Context` methods for future events
+type Callback = dyn FnOnce(&mut Context);
+
+/// A manager for the state of a discrete-event simulation
+///
+/// Provides core simulation services including
+/// * Maintaining a notion of time
+/// * Scheduling events to occur at some point in the future and executing them
+///   at that time
+/// * Holding data that can be accessed by simulation modules
+///
+/// Simulations are constructed out of a series of interacting modules that
+/// take turns manipulating the Context through a mutable reference. Modules
+/// store data in the simulation using the `DataPlugin` trait that allows them
+/// to retrieve data by type.
+///
+/// The future event list of the simulation is a queue of `Callback` objects -
+/// called `plans` - that will assume control of the Context at a future point
+/// in time and execute the logic in the associated `FnOnce(&mut Context)`
+/// closure. Modules can add plans to this queue through the `Context`.
+///
+/// The simulation also has a separate callback mechanism. Callbacks
+/// fire before the next timed event (even if it is scheduled for the
+/// current time. This allows modules to schedule actions for immediate
+/// execution but outside of the current iteration of the event loop.
+///
+pub struct Context {
+    plan_queue: Queue<Box<Callback>>,
+    callback_queue: VecDeque<Box<Callback>>,
+    data_plugins: HashMap<TypeId, Box<dyn Any>>,
+    current_time: f64,
+}
+
+impl Context {
+    /// Create a new empty `Context`
+    #[must_use]
+    pub fn new() -> Context {
+        Context {
+            plan_queue: Queue::new(),
+            callback_queue: VecDeque::new(),
+            data_plugins: HashMap::new(),
+            current_time: 0.0,
+        }
+    }
+
+    /// Add a plan to the future event list at the specified time
+    ///
+    /// Returns an `Id` for the newly-added plan that can be used to cancel it
+    /// if needed.
+    /// # Panics
+    ///
+    /// Panics if time is in the past, infinite, or NaN.
+    pub fn add_plan(&mut self, time: f64, callback: impl FnOnce(&mut Context) + 'static) -> Id {
+        assert!(!time.is_nan() && !time.is_infinite() && time >= self.current_time);
+        self.plan_queue.add_plan(time, Box::new(callback))
+    }
+
+    /// Cancel a plan that has been added to the queue
+    ///
+    /// # Panics
+    ///
+    /// This function panics if you cancel a plan which has already been
+    /// cancelled or executed.
+    pub fn cancel_plan(&mut self, id: &Id) {
+        self.plan_queue.cancel_plan(id);
+    }
+
+    /// Add a `Callback` to the queue to be executed before the next plan
+    pub fn queue_callback(&mut self, callback: impl FnOnce(&mut Context) + 'static) {
+        self.callback_queue.push_back(Box::new(callback));
+    }
+
+    /// Retrieve a mutable reference to the data container associated with a
+    /// `DataPlugin`
+    ///
+    /// If the data container has not been already added to the `Context` then
+    /// this function will use the `DataPlugin::create_data_container` method
+    /// to construct a new data container and store it in the `Context`.
+    ///
+    /// Returns a mutable reference to the data container
+    #[must_use]
+    #[allow(clippy::missing_panics_doc)]
+    pub fn get_data_container_mut<T: DataPlugin>(&mut self) -> &mut T::DataContainer {
+        self.data_plugins
+            .entry(TypeId::of::<T>())
+            .or_insert_with(|| Box::new(T::create_data_container()))
+            .downcast_mut::<T::DataContainer>()
+            .unwrap() // Will never panic as data container has the matching type
+    }
+
+    /// Retrieve a reference to the data container associated with a
+    /// `DataPlugin`
+    ///
+    /// Returns a reference to the data container if it exists or else `None`
+    #[must_use]
+    pub fn get_data_container<T: DataPlugin>(&self) -> Option<&T::DataContainer> {
+        if let Some(data) = self.data_plugins.get(&TypeId::of::<T>()) {
+            data.downcast_ref::<T::DataContainer>()
+        } else {
+            None
+        }
+    }
+
+    /// Get the current time in the simulation
+    ///
+    /// Returns the current time
+    #[must_use]
+    pub fn get_current_time(&self) -> f64 {
+        self.current_time
+    }
+
+    /// Execute the simulation until the plan and callback queues are empty
+    pub fn execute(&mut self) {
+        // Start plan loop
+        loop {
+            // If there is a callback, run it.
+            if let Some(callback) = self.callback_queue.pop_front() {
+                callback(self);
+                continue;
+            }
+
+            // There aren't any callbacks, so look at the first plan.
+            if let Some(plan) = self.plan_queue.get_next_plan() {
+                self.current_time = plan.time;
+                (plan.data)(self);
+            } else {
+                // OK, there aren't any plans, so we're done.
+                break;
+            }
+        }
+    }
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// A trait for objects that can provide data containers to be held by `Context`
+pub trait DataPlugin: Any {
+    type DataContainer;
+
+    fn create_data_container() -> Self::DataContainer;
+}
+
+#[macro_export]
+macro_rules! define_data_plugin {
+    ($plugin:ident, $data_container:ty, $default: expr) => {
+        struct $plugin {}
+
+        impl $crate::context::DataPlugin for $plugin {
+            type DataContainer = $data_container;
+
+            fn create_data_container() -> Self::DataContainer {
+                $default
+            }
+        }
+    };
+}
+pub use define_data_plugin;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    define_data_plugin!(ComponentA, Vec<u32>, vec![]);
+
+    #[test]
+    fn empty_context() {
+        let mut context = Context::new();
+        context.execute();
+        assert_eq!(context.get_current_time(), 0.0);
+    }
+
+    #[test]
+    fn get_data_container() {
+        let mut context = Context::new();
+        context.get_data_container_mut::<ComponentA>().push(1);
+        assert_eq!(
+            *context.get_data_container::<ComponentA>().unwrap(),
+            vec![1],
+        );
+    }
+
+    #[test]
+    fn get_uninitialized_data_container() {
+        let context = Context::new();
+        assert!(context.get_data_container::<ComponentA>().is_none());
+    }
+
+    fn add_plan(context: &mut Context, time: f64, value: u32) -> Id {
+        context.add_plan(time, move |context| {
+            context.get_data_container_mut::<ComponentA>().push(value);
+        })
+    }
+
+    #[test]
+    #[should_panic]
+    fn negative_plan_time() {
+        let mut context = Context::new();
+        add_plan(&mut context, -1.0, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn infinite_plan_time() {
+        let mut context = Context::new();
+        add_plan(&mut context, f64::INFINITY, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn nan_plan_time() {
+        let mut context = Context::new();
+        add_plan(&mut context, f64::NAN, 0);
+    }
+
+    #[test]
+    fn timed_plan_only() {
+        let mut context = Context::new();
+        add_plan(&mut context, 1.0, 1);
+        context.execute();
+        assert_eq!(context.get_current_time(), 1.0);
+        assert_eq!(*context.get_data_container_mut::<ComponentA>(), vec![1]);
+    }
+
+    #[test]
+    fn callback_only() {
+        let mut context = Context::new();
+        context.queue_callback(|context| {
+            context.get_data_container_mut::<ComponentA>().push(1);
+        });
+        context.execute();
+        assert_eq!(context.get_current_time(), 0.0);
+        assert_eq!(*context.get_data_container_mut::<ComponentA>(), vec![1]);
+    }
+
+    #[test]
+    fn callback_before_timed_plan() {
+        let mut context = Context::new();
+        context.queue_callback(|context| {
+            context.get_data_container_mut::<ComponentA>().push(1);
+        });
+        add_plan(&mut context, 1.0, 2);
+        context.execute();
+        assert_eq!(context.get_current_time(), 1.0);
+        assert_eq!(*context.get_data_container_mut::<ComponentA>(), vec![1, 2]);
+    }
+
+    #[test]
+    fn callback_adds_timed_plan() {
+        let mut context = Context::new();
+        context.queue_callback(|context| {
+            context.get_data_container_mut::<ComponentA>().push(1);
+            add_plan(context, 1.0, 2);
+            context.get_data_container_mut::<ComponentA>().push(3);
+        });
+        context.execute();
+        assert_eq!(context.get_current_time(), 1.0);
+        assert_eq!(
+            *context.get_data_container_mut::<ComponentA>(),
+            vec![1, 3, 2]
+        );
+    }
+
+    #[test]
+    fn callback_adds_callback_and_timed_plan() {
+        let mut context = Context::new();
+        context.queue_callback(|context| {
+            context.get_data_container_mut::<ComponentA>().push(1);
+            add_plan(context, 1.0, 2);
+            context.queue_callback(|context| {
+                context.get_data_container_mut::<ComponentA>().push(4);
+            });
+            context.get_data_container_mut::<ComponentA>().push(3);
+        });
+        context.execute();
+        assert_eq!(context.get_current_time(), 1.0);
+        assert_eq!(
+            *context.get_data_container_mut::<ComponentA>(),
+            vec![1, 3, 4, 2]
+        );
+    }
+
+    #[test]
+    fn timed_plan_adds_callback_and_timed_plan() {
+        let mut context = Context::new();
+        context.add_plan(1.0, |context| {
+            context.get_data_container_mut::<ComponentA>().push(1);
+            // We add the plan first, but the callback will fire first.
+            add_plan(context, 2.0, 3);
+            context.queue_callback(|context| {
+                context.get_data_container_mut::<ComponentA>().push(2);
+            });
+        });
+        context.execute();
+        assert_eq!(context.get_current_time(), 2.0);
+        assert_eq!(
+            *context.get_data_container_mut::<ComponentA>(),
+            vec![1, 2, 3]
+        );
+    }
+
+    #[test]
+    fn cancel_plan() {
+        let mut context = Context::new();
+        let to_cancel = add_plan(&mut context, 2.0, 1);
+        context.add_plan(1.0, move |context| {
+            context.cancel_plan(&to_cancel);
+        });
+        context.execute();
+        assert_eq!(context.get_current_time(), 1.0);
+        assert_eq!(*context.get_data_container_mut::<ComponentA>(), vec![]);
+    }
+
+    #[test]
+    fn add_plan_with_current_time() {
+        let mut context = Context::new();
+        context.add_plan(1.0, move |context| {
+            context.get_data_container_mut::<ComponentA>().push(1);
+            add_plan(context, 1.0, 2);
+            context.queue_callback(|context| {
+                context.get_data_container_mut::<ComponentA>().push(3);
+            });
+        });
+        context.execute();
+        assert_eq!(context.get_current_time(), 1.0);
+        assert_eq!(
+            *context.get_data_container_mut::<ComponentA>(),
+            vec![1, 3, 2]
+        );
+    }
+
+    #[test]
+    fn plans_at_same_time_fire_in_order() {
+        let mut context = Context::new();
+        add_plan(&mut context, 1.0, 1);
+        add_plan(&mut context, 1.0, 2);
+        context.execute();
+        assert_eq!(context.get_current_time(), 1.0);
+        assert_eq!(*context.get_data_container_mut::<ComponentA>(), vec![1, 2]);
+    }
+}
---
+++ src/plan.rs
@@ -0,0 +1,238 @@
+//! A priority queue that stores arbitrary data sorted by time
+//!
+//! Defines a `Queue<T>` that is intended to store a queue of items of type T,
+//! sorted by `f64` time, called 'plans'. This queue has methods for adding
+//! plans, cancelling plans, and retrieving the earliest plan in the queue.
+//! Adding a plan is *O*(log(*n*)) while cancellation and retrieval are *O*(1).
+//!
+//! This queue is used by `Context` to store future events where some callback
+//! closure `FnOnce(&mut Context)` will be executed at a given point in time.
+
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashMap},
+};
+
+/// A priority queue that stores arbitrary data sorted by time
+///
+/// Items of type `T` are stored in order by `f64` time and called `Plan<T>`.
+/// When plans are created they are sequentially assigned an `Id` that is a
+/// wrapped `u64`. If two plans are scheduled for the same time the plan that is
+/// scheduled first (i.e., that has the lowest id) is placed earlier.
+///
+/// The pair of time and plan id are stored in a binary heap of `Entry` objects.
+/// The data payload of the event is stored in a hash map by plan id.
+/// Plan cancellation occurs by removing the corresponding entry from the data
+/// hash map.
+pub struct Queue<T> {
+    queue: BinaryHeap<Entry>,
+    data_map: HashMap<u64, T>,
+    plan_counter: u64,
+}
+
+impl<T> Queue<T> {
+    /// Create a new empty `Queue<T>`
+    #[must_use]
+    pub fn new() -> Queue<T> {
+        Queue {
+            queue: BinaryHeap::new(),
+            data_map: HashMap::new(),
+            plan_counter: 0,
+        }
+    }
+
+    /// Add a plan to the queue at the specified time
+    ///
+    /// Returns an `Id` for the newly-added plan that can be used to cancel it
+    /// if needed.
+    pub fn add_plan(&mut self, time: f64, data: T) -> Id {
+        // Add plan to queue, store data, and increment counter
+        let id = self.plan_counter;
+        self.queue.push(Entry { time, id });
+        self.data_map.insert(id, data);
+        self.plan_counter += 1;
+        Id { id }
+    }
+
+    /// Cancel a plan that has been added to the queue
+    ///
+    /// # Panics
+    ///
+    /// This function panics if you cancel a plan which has already
+    /// been cancelled or executed.
+    pub fn cancel_plan(&mut self, id: &Id) {
+        // Delete the plan from the map, but leave in the queue
+        // It will be skipped when the plan is popped from the queue
+        self.data_map.remove(&id.id).expect("Plan does not exist");
+    }
+
+    /// Retrieve the earliest plan in the queue
+    ///
+    /// Returns the next plan if it exists or else `None` if the queue is empty
+    pub fn get_next_plan(&mut self) -> Option<Plan<T>> {
+        loop {
+            // Pop from queue until we find a plan with data or queue is empty
+            match self.queue.pop() {
+                Some(entry) => {
+                    // Skip plans that have been cancelled and thus have no data
+                    if let Some(data) = self.data_map.remove(&entry.id) {
+                        return Some(Plan {
+                            time: entry.time,
+                            data,
+                        });
+                    }
+                }
+                None => {
+                    return None;
+                }
+            }
+        }
+    }
+}
+
+impl<T> Default for Queue<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// A time and id pair used to order plans in the `Queue<T>`
+///
+/// `Entry` objects are sorted in increasing order of time and then plan id
+#[derive(PartialEq, Debug)]
+struct Entry {
+    time: f64,
+    id: u64,
+}
+
+impl Eq for Entry {}
+
+impl PartialOrd for Entry {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+/// Entry objects are ordered in increasing order by time and then plan id
+impl Ord for Entry {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let time_ordering = self.time.partial_cmp(&other.time).unwrap().reverse();
+        match time_ordering {
+            // Break time ties in order of plan id
+            Ordering::Equal => self.id.cmp(&other.id).reverse(),
+            _ => time_ordering,
+        }
+    }
+}
+
+/// A unique identifier for a plan added to a `Queue<T>`
+pub struct Id {
+    id: u64,
+}
+
+/// A plan that holds data of type `T` intended to be used at the specified time
+pub struct Plan<T> {
+    pub time: f64,
+    pub data: T,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Queue;
+
+    #[test]
+    fn empty_queue() {
+        let mut plan_queue = Queue::<()>::new();
+        assert!(plan_queue.get_next_plan().is_none());
+    }
+
+    #[test]
+    fn add_plans() {
+        let mut plan_queue = Queue::new();
+        plan_queue.add_plan(1.0, 1);
+        plan_queue.add_plan(3.0, 3);
+        plan_queue.add_plan(2.0, 2);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 1.0);
+        assert_eq!(next_plan.data, 1);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 2.0);
+        assert_eq!(next_plan.data, 2);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 3.0);
+        assert_eq!(next_plan.data, 3);
+
+        assert!(plan_queue.get_next_plan().is_none());
+    }
+
+    #[test]
+    fn add_plans_at_same_time() {
+        let mut plan_queue = Queue::new();
+        plan_queue.add_plan(1.0, 1);
+        plan_queue.add_plan(1.0, 2);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 1.0);
+        assert_eq!(next_plan.data, 1);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 1.0);
+        assert_eq!(next_plan.data, 2);
+
+        assert!(plan_queue.get_next_plan().is_none());
+    }
+
+    #[test]
+    fn add_and_cancel_plans() {
+        let mut plan_queue = Queue::new();
+        plan_queue.add_plan(1.0, 1);
+        let plan_to_cancel = plan_queue.add_plan(2.0, 2);
+        plan_queue.add_plan(3.0, 3);
+        plan_queue.cancel_plan(&plan_to_cancel);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 1.0);
+        assert_eq!(next_plan.data, 1);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 3.0);
+        assert_eq!(next_plan.data, 3);
+
+        assert!(plan_queue.get_next_plan().is_none());
+    }
+
+    #[test]
+    fn add_and_get_plans() {
+        let mut plan_queue = Queue::new();
+        plan_queue.add_plan(1.0, 1);
+        plan_queue.add_plan(2.0, 2);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 1.0);
+        assert_eq!(next_plan.data, 1);
+
+        plan_queue.add_plan(1.5, 3);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 1.5);
+        assert_eq!(next_plan.data, 3);
+
+        let next_plan = plan_queue.get_next_plan().unwrap();
+        assert_eq!(next_plan.time, 2.0);
+        assert_eq!(next_plan.data, 2);
+
+        assert!(plan_queue.get_next_plan().is_none());
+    }
+
+    #[test]
+    #[should_panic]
+    fn cancel_invalid_plan() {
+        let mut plan_queue = Queue::<()>::new();
+        let plan_to_cancel = plan_queue.add_plan(1.0, ());
+        plan_queue.get_next_plan();
+        plan_queue.cancel_plan(&plan_to_cancel);
+    }
+}
