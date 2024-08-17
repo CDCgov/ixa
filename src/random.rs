a776e218a66168ca067dc027a00a14d5bc8d80cb
Jason Asher <swo9@cdc.gov>
2024-08-17 13:47:54 -0400

    Example random sampling using closures

--- src/random.rs
+++ src/random.rs
@@ -53,6 +53,13 @@ pub trait RandomContext {
     fn set_base_random_seed(&mut self, base_seed: u64);
 
     fn get_rng<R: RngId>(&self) -> RefMut<'_, R::RngType>;
+
+    fn sample<R: RngId, T>(&self, sampler: impl FnOnce(&mut R::RngType) -> T) -> T;
+
+    fn sample_with_context<R: RngId, T>(
+        &self,
+        sampler: impl FnOnce(&mut R::RngType, &Context) -> T,
+    ) -> T;
 }
 
 impl RandomContext for Context {
@@ -93,13 +100,74 @@ impl RandomContext for Context {
                 .unwrap()
         })
     }
+
+    // Alternative to the 'get_rng' approach that hides interior mutability
+    // See the tests below as the caller must specify both generic params R and T
+    fn sample<R: RngId, T>(&self, sampler: impl FnOnce(&mut R::RngType) -> T) -> T {
+        let data_container = self
+            .get_data_container::<RngPlugin>()
+            .expect("You must initialize the random number generator with a base seed");
+
+        let rng_holders = data_container.rng_holders.try_borrow_mut().unwrap();
+
+        let mut rng = RefMut::map(rng_holders, |holders| {
+            holders
+                .entry(TypeId::of::<R>())
+                // Create a new rng holder if it doesn't exist yet
+                .or_insert_with(|| {
+                    let base_seed = data_container.base_seed;
+                    let seed_offset = fxhash::hash64(R::get_name());
+                    RngHolder {
+                        rng: Box::new(R::RngType::seed_from_u64(base_seed + seed_offset)),
+                    }
+                })
+                .rng
+                .downcast_mut::<R::RngType>()
+                .unwrap()
+        });
+
+        sampler(&mut rng)
+    }
+
+    // An additional option that makes it possible for the sampler to retrieve
+    // data from Context that may be needed without capturing variables from its
+    // environment
+    fn sample_with_context<R: RngId, T>(
+        &self,
+        sampler: impl FnOnce(&mut R::RngType, &Context) -> T,
+    ) -> T {
+        let data_container = self
+            .get_data_container::<RngPlugin>()
+            .expect("You must initialize the random number generator with a base seed");
+
+        let rng_holders = data_container.rng_holders.try_borrow_mut().unwrap();
+
+        let mut rng = RefMut::map(rng_holders, |holders| {
+            holders
+                .entry(TypeId::of::<R>())
+                // Create a new rng holder if it doesn't exist yet
+                .or_insert_with(|| {
+                    let base_seed = data_container.base_seed;
+                    let seed_offset = fxhash::hash64(R::get_name());
+                    RngHolder {
+                        rng: Box::new(R::RngType::seed_from_u64(base_seed + seed_offset)),
+                    }
+                })
+                .rng
+                .downcast_mut::<R::RngType>()
+                .unwrap()
+        });
+
+        sampler(&mut rng, self)
+    }
 }
 
 #[cfg(test)]
 mod test {
     use crate::context::Context;
+    use crate::define_data_plugin;
     use crate::random::RandomContext;
-    use rand::RngCore;
+    use rand::{distributions::WeightedIndex, prelude::Distribution, RngCore};
 
     define_rng!(FooRng);
     define_rng!(BarRng);
@@ -170,5 +238,67 @@ mod test {
         let mut foo_rng = context.get_rng::<FooRng>();
         assert_ne!(run_0, foo_rng.next_u64());
         assert_ne!(run_1, foo_rng.next_u64());
+    }
+
+    #[test]
+    fn sampler_function() {
+        let mut context = Context::new();
+        context.set_base_random_seed(42);
+
+        // Note: Ergonomics are not ideal because we have to specify the return
+        // type in the turbofish (can't do sample::<FooRng> and have the return
+        // be inferred)
+        let x = context.sample::<FooRng, u32>(|rng| rng.next_u32());
+        let y = context.sample::<FooRng, u32>(|rng| rng.next_u32());
+        assert_ne!(x, y);
+    }
+
+    define_data_plugin!(
+        SamplerData,
+        WeightedIndex<f64>,
+        WeightedIndex::new(vec![1.0]).unwrap()
+    );
+
+    #[test]
+    fn sampler_function_closure_capture() {
+        let mut context = Context::new();
+        context.set_base_random_seed(42);
+        // Initialize normal parameters
+        *context.get_data_container_mut::<SamplerData>() =
+            WeightedIndex::new(vec![1.0, 2.0]).unwrap();
+
+        let parameters = context.get_data_container::<SamplerData>().unwrap();
+        let n_samples = 3000;
+        let mut zero_counter = 0;
+        for _ in 0..n_samples {
+            let sample = context.sample::<FooRng, usize>(|rng| parameters.sample(rng));
+            if sample == 0 {
+                zero_counter += 1;
+            }
+        }
+        assert!((zero_counter - 1000 as i32).abs() < 30);
+    }
+
+    #[test]
+    fn sampler_function_with_context() {
+        let mut context = Context::new();
+        context.set_base_random_seed(42);
+        // Initialize normal parameters
+        *context.get_data_container_mut::<SamplerData>() =
+            WeightedIndex::new(vec![1.0, 2.0]).unwrap();
+
+        let n_samples = 3000;
+        let mut zero_counter = 0;
+        for _ in 0..n_samples {
+            // Same ergonomic issue as with sampler without context
+            let sample = context.sample_with_context::<FooRng, usize>(|rng, context| {
+                let parameters = context.get_data_container::<SamplerData>().unwrap();
+                parameters.sample(rng)
+            });
+            if sample == 0 {
+                zero_counter += 1;
+            }
+        }
+        assert!((zero_counter - 1000 as i32).abs() < 30);
     }
 }
