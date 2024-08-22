34e7f84888cb5ab6d660e3d6e3eacaace16d8b39
ranya-cfa <xsq2@cdc.gov>
2024-08-22 16:15:12 -0500

    Report Example for Multi-Threading (#26)
    * example for multi-threaded
    
    * making multi-threaded simpler
    
    * running precommit
    
    * removing unneccesary clones, adding scenario to log
    
    * adding struct definition

--- examples/reports-multi-threaded/main.rs
+++ examples/reports-multi-threaded/main.rs
@@ -1,6 +1,12 @@
 use ixa::context::Context;
 use std::thread;
 
+struct Incidence {
+    scenario: String,
+    person_id: String,
+    t: f64,
+}
+
 fn main() {
     let scenarios = vec!["Illinois", "Wisconsin", "Arizona", "California"];
     let mut handles = vec![];
@@ -8,10 +14,34 @@ fn main() {
     for scenario in scenarios {
         let scenario = scenario.to_string();
         let handle = thread::spawn(move || {
-            // Replace this with the actual example code, similar to the simple
-            // example in examples/reports/main.rs
             let mut context = Context::new();
+
+            #[cfg(feature = "reports")]
+            context.add_report::<Incidence>("Incidence");
+
             println!("Scenario: {}", scenario);
+
+            let people = vec!["1", "2", "3"];
+            for person in people {
+                let person = person.to_string();
+                let scenario = scenario.clone();
+                context.add_plan(1.0, {
+                    move |context| {
+                        #[cfg(feature = "reports")]
+                        context.send_report(Incidence {
+                            scenario: scenario,
+                            person_id: person.clone(),
+                            t: context.get_current_time(),
+                        });
+                        println!(
+                            "Scenario: {}, Person {} was infected at time {}.",
+                            scenario,
+                            person,
+                            context.get_current_time()
+                        );
+                    }
+                });
+            }
 
             context.execute();
         });
