bbab36d242f170935d96b3bea11511270b276ebd
Guido Espana <ukd0@cdc.gov>
2024-08-28 15:07:14 -0400

    Testing recovery in infection_manager

--- examples/basic-infection/infection_manager.rs
+++ examples/basic-infection/infection_manager.rs
@@ -26,11 +26,7 @@ fn schedule_recovery(context: &mut Context, person_id: usize) {
 }
 
 fn handle_infection_status_change(context: &mut Context, event: InfectionStatusEvent) {
-    if matches!(
-        //context.get_person_status(event.person_id),
-        event.updated_status,
-        InfectionStatus::I
-    ) {
+    if matches!(event.updated_status, InfectionStatus::I) {
         schedule_recovery(context, event.person_id);
     }
 }
@@ -43,24 +39,45 @@ impl InfectionManager for Context {
     }
 }
 
-/*
 #[cfg(test)]
 mod test {
+    use crate::infection_manager::InfectionManager;
+    use crate::people::InfectionStatus;
+    use crate::people::InfectionStatusEvent;
+    use crate::people::PeopleContext;
     use ixa::context::Context;
+    use ixa::define_data_plugin;
+    use ixa::define_rng;
+    use ixa::random::ContextRandomExt;
+
+    define_data_plugin!(RecoveryPlugin, usize, 0);
+
+    fn handle_recovery_event(context: &mut Context, event: InfectionStatusEvent) {
+        if matches!(event.updated_status, InfectionStatus::R) {
+            *context.get_data_container_mut(RecoveryPlugin) += 1;
+        }
+    }
 
     #[test]
     fn test_handle_infection_change() {
         use super::handle_infection_status_change;
         let mut context = Context::new();
-        context.create_person();
+        context.init_random(42);
+        context.initialize_infection_manager();
 
-        let person_id = 0;
-        let person_status: InfectionStatus = context.get_person_status(person_id);
-        assert_eq!(person_status, InfectionStatus::S);
+        context.subscribe_to_event::<InfectionStatusEvent>(move |context, event| {
+            handle_recovery_event(context, event);
+        });
 
-        context.set_person_status(person_id, InfectionStatus::I);
-        assert_eq!(context.get_person_status(person_id), InfectionStatus::I);
+        let population_size = 10;
+        for id in 0..population_size {
+            context.create_person();
+            context.set_person_status(id, InfectionStatus::I);
+        }
+
+        context.execute();
+        let recovered_size: usize = *context.get_data_container(RecoveryPlugin).unwrap();
+
+        assert_eq!(recovered_size, population_size);
     }
-
 }
-*/
