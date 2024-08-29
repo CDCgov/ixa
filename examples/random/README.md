cd108f7dc1a55499eec10f2b8a9869321bac5087
ranya-cfa <xsq2@cdc.gov>
2024-08-29 14:48:14 -0400

    create random example

---
+++ examples/random/README.md
@@ -0,0 +1,4 @@
+# Random - Simple Example
+
+This example demonstrates randomly selecting a person from a population and
+scheduling for them to recover at a random time.
---
+++ examples/random/main.rs
@@ -0,0 +1,37 @@
+use ixa::context::Context;
+use ixa::define_rng;
+use ixa::random::ContextRandomExt;
+use rand::distributions::Uniform;
+
+static SEED: u64 = 123;
+static POPULATION: u64 = 10;
+
+define_rng!(MyRng);
+
+fn main() {
+    let mut context = Context::new();
+    context.init_random(SEED);
+
+    let random_person = context.sample_range(MyRng, 0..POPULATION);
+    let person_id = random_person.to_string();
+
+    context.add_plan(1.0, {
+        let person_id = person_id.clone();
+        move |context| {
+            println!(
+                "Person {} was infected at time {}",
+                person_id,
+                context.get_current_time()
+            );
+        }
+    });
+
+    let recovery_time: f64 = context.sample_distr(MyRng, Uniform::new(2.0, 10.0));
+    context.add_plan(1.0, {
+        let person_id = person_id.clone();
+        move |_context| {
+            println!("Person {person_id} recovered at time {recovery_time}");
+        }
+    });
+    context.execute();
+}
