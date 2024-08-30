5d68869c0f2adb3f7c5954dd3b225b744561388a
ranya-cfa <xsq2@cdc.gov>
2024-08-30 09:56:33 -0400

    adding custom

--- examples/random/main.rs
+++ examples/random/main.rs
@@ -2,6 +2,8 @@ use ixa::context::Context;
 use ixa::define_rng;
 use ixa::random::ContextRandomExt;
 use rand::distributions::Uniform;
+use rand::{distributions::WeightedIndex, prelude::Distribution};
+use rand::Rng;
 
 static SEED: u64 = 123;
 static POPULATION: u64 = 10;
@@ -13,22 +15,30 @@ fn main() {
     context.init_random(SEED);
 
     let random_person = context.sample_range(MyRng, 0..POPULATION);
-    let person_id = random_person;
 
     context.add_plan(1.0, {
         move |context| {
             println!(
                 "Person {} was infected at time {}",
-                person_id,
+                random_person,
                 context.get_current_time()
             );
         }
     });
 
-    let recovery_time: f64 = context.sample_distr(MyRng, Uniform::new(2.0, 10.0));
-    context.add_plan(1.0, {
-        move |_context| {
-            println!("Person {person_id} recovered at time {recovery_time}");
+    let recovery_time = if context.sample_bool(MyRng, 0.5) {
+        context.sample_distr(MyRng, Uniform::new(2.0, 10.0))
+    }
+    else {
+        let dist = WeightedIndex::new(vec![1.0, 2.0]).unwrap();
+        let i = context.sample(MyRng, |rng| dist.sample(rng));
+        //println!("{i}");
+        //context.sample(MyRng, |rng| rng.next_f64())
+    };
+    
+    context.add_plan(recovery_time, {
+        move |context| {
+            println!("Person {random_person} recovered at time {}", context.get_current_time());
         }
     });
     context.execute();
