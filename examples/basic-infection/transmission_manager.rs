f0958162c0fea751e3fc6f7b8a3afdbb059e24f2
Guido Espana <confunguido@gmail.com>
2024-09-09 14:41:32 -0400

    Expanded description of per-person FOI

--- examples/basic-infection/transmission_manager.rs
+++ examples/basic-infection/transmission_manager.rs
@@ -19,9 +19,15 @@ fn attempt_infection(context: &mut Context) {
 
     if matches!(person_status, InfectionStatus::S) {
         context.set_person_status(person_to_infect, InfectionStatus::I);
-        //context.schedule_recovery(person_to_infect);
     }
-    //Here, we use a per-person force of infection (FOI) derived from the population-level force of infection to represent a constant risk of disease for individuals in the population.
+
+    // With a food-borne illness (i.e., constant force of infection),
+    // each _person_ experiences an exponentially distributed
+    // time until infected. Here, we use a per-person force of infection derived from the population-level to represent a constant risk of infection for individuals in the population.
+
+    // An alternative implementation calculates each person's time to infection
+    // at the beginning of the simulation and scheudles their infection at that time.
+
     #[allow(clippy::cast_precision_loss)]
     let next_attempt_time = context.get_current_time()
         + context.sample_distr(TransmissionRng, Exp::new(FOI).unwrap()) / population_size as f64;
