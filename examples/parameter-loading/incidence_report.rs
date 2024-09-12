324deb7e1cbaf37706410e65d134a7138668cc64
Guido Espana <confunguido@gmail.com>
2024-09-12 16:44:32 -0400

    Return references from global properties instead of copying

--- examples/parameter-loading/incidence_report.rs
+++ examples/parameter-loading/incidence_report.rs
@@ -2,10 +2,13 @@ use ixa::context::Context;
 use ixa::report::ContextReportExt;
 use ixa::{create_report_trait, report::Report};
 use std::path::PathBuf;
+use ixa::global_properties::ContextGlobalPropertiesExt;
 
 use crate::people::InfectionStatus;
 use crate::people::InfectionStatusEvent;
 use serde::{Deserialize, Serialize};
+
+use crate::Parameters;
 
 #[derive(Serialize, Deserialize, Clone)]
 struct IncidenceReportItem {
@@ -25,10 +28,11 @@ fn handle_infection_status_change(context: &mut Context, event: InfectionStatusE
 }
 
 pub fn init(context: &mut Context) {
+    let parameters = context.get_global_property_value(Parameters).clone();
     context
         .report_options()
-        .directory(PathBuf::from("./examples/parameter-loading/"));
-    context.add_report::<IncidenceReportItem>("incidence");
+        .directory(PathBuf::from(parameters.output_dir));
+    context.add_report::<IncidenceReportItem>(&parameters.output_file);
     context.subscribe_to_event::<InfectionStatusEvent>(|context, event| {
         handle_infection_status_change(context, event);
     });
