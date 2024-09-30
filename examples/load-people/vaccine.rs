78125da282ae41a633a6434ff3423ff2b8953906
k88hudson-cfa <ryl8@cdc.gov>
2024-09-30 19:05:06 -0400

    Use OrderedFloat for floats

--- examples/load-people/vaccine.rs
+++ examples/load-people/vaccine.rs
@@ -1,10 +1,11 @@
-use std::hash::{Hash, Hasher};
+use std::hash::Hash;
 
 use crate::population_loader::{Age, RiskCategory};
 use ixa::{
     context::Context, define_person_property, define_rng, people::ContextPeopleExt,
     random::ContextRandomExt,
 };
+use ordered_float::OrderedFloat;
 
 define_rng!(VaccineRng);
 
@@ -15,18 +16,8 @@ pub enum VaccineTypeValue {
     B,
 }
 
-#[allow(clippy::module_name_repetitions)]
-#[derive(Copy, Clone, Debug)]
-pub struct VaccineEfficacyValue(pub f64);
-
-impl Hash for VaccineEfficacyValue {
-    fn hash<H: Hasher>(&self, _state: &mut H) {
-        // TODO(ryl8@cdc.gov): Hashing for floats?
-        panic!("Unimplemented")
-    }
-}
 define_person_property!(VaccineType, VaccineTypeValue);
-define_person_property!(VaccineEfficacy, VaccineEfficacyValue);
+define_person_property!(VaccineEfficacy, OrderedFloat<f64>);
 define_person_property!(VaccineDoses, u8, |context: &Context, person_id| {
     let age = context.get_person_property(person_id, Age);
     if age > 10 {
@@ -37,18 +28,18 @@ define_person_property!(VaccineDoses, u8, |context: &Context, person_id| {
 });
 
 pub trait ContextVaccineExt {
-    fn get_vaccine_props(&self, risk: RiskCategory) -> (VaccineTypeValue, VaccineEfficacyValue);
+    fn get_vaccine_props(&self, risk: RiskCategory) -> (VaccineTypeValue, OrderedFloat<f64>);
 }
 
 impl ContextVaccineExt for Context {
     fn get_vaccine_props(
         self: &Context,
         risk: RiskCategory,
-    ) -> (VaccineTypeValue, VaccineEfficacyValue) {
+    ) -> (VaccineTypeValue, OrderedFloat<f64>) {
         if risk == RiskCategory::High {
-            (VaccineTypeValue::A, VaccineEfficacyValue(0.9))
+            (VaccineTypeValue::A, OrderedFloat(0.9))
         } else {
-            (VaccineTypeValue::B, VaccineEfficacyValue(0.8))
+            (VaccineTypeValue::B, OrderedFloat(0.8))
         }
     }
 }
