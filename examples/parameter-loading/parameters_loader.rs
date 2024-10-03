7f8f5e78a1a7335737456ba2a6bd5b209bce4551
Guido Espana <confunguido@gmail.com>
2024-10-03 16:57:01 +0000

    Moving parameter loading to global properties so we can access from context

--- examples/parameter-loading/parameters_loader.rs
+++ examples/parameter-loading/parameters_loader.rs
@@ -1,11 +1,9 @@
 use ixa::context::Context;
 use ixa::global_properties::ContextGlobalPropertiesExt;
 use std::fmt::Debug;
-use std::fs;
 
 use ixa::define_global_property;
 
-use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -20,16 +18,8 @@ pub struct ParametersValues {
 }
 define_global_property!(Parameters, ParametersValues);
 
-fn load_parameters_from_config<T: 'static + Debug + DeserializeOwned>(
-    _context: &mut Context,
-    file_name: &str,
-) -> T {
-    let config_file = fs::read_to_string(file_name).expect("Config file not loaded properly");
-    let config: T = toml::from_str(&config_file).expect("could not parse config file as toml");
-    config
-}
-
 pub fn init_parameters(context: &mut Context, file_name: &str) {
-    let parameters_values = load_parameters_from_config::<ParametersValues>(context, file_name);
+    let parameters_values = context
+        .load_parameters_from_config::<ParametersValues>(file_name);
     context.set_global_property_value(Parameters, parameters_values);
 }
