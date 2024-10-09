42c27f9c2cd69f0e17dd5aa81162cfbcc36061a4
Guido Espana <confunguido@gmail.com>
2024-10-09 15:57:46 +0000

    IxaError to manage parameter loading from json files

--- examples/parameter-loading/main.rs
+++ examples/parameter-loading/main.rs
@@ -30,22 +30,28 @@ fn main() {
     let file_path = Path::new("examples")
         .join("parameter-loading")
         .join("input.json");
-    parameters_loader::init_parameters(&mut context, &file_path);
+    
+    match  parameters_loader::init_parameters(&mut context, &file_path) {
+        Ok(_) => {
+            let parameters = context.get_global_property_value(Parameters).clone();
+            context.init_random(parameters.seed);
 
-    let parameters = context.get_global_property_value(Parameters).clone();
-    context.init_random(parameters.seed);
+            for _ in 0..parameters.population {
+                context.add_person();
+            }
 
-    for _ in 0..parameters.population {
-        context.add_person();
+            transmission_manager::init(&mut context);
+            infection_manager::init(&mut context);
+            incidence_report::init(&mut context);
+
+            context.add_plan(parameters.max_time, |context| {
+                context.shutdown();
+            });
+            println!("{parameters:?}");
+            context.execute();
+        },
+        Err(ixa_error) => {
+            println!("Could not read parameters: {}", ixa_error);
+        }
     }
-
-    transmission_manager::init(&mut context);
-    infection_manager::init(&mut context);
-    incidence_report::init(&mut context);
-
-    context.add_plan(parameters.max_time, |context| {
-        context.shutdown();
-    });
-    println!("{parameters:?}");
-    context.execute();
 }
