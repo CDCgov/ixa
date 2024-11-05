59b6296386026297788de7fe9ab8edfa114ceab0
Guido Espana <ukd0@cdc.gov>
2024-11-05 22:57:45 +0000

    Added skeleton for synth pop example

---
+++ examples/synth-pop/population_report.rs
@@ -0,0 +1,54 @@
+use crate::population_manager::{Age, VaccineAgeGroup, AgeGroupRisk, Alive};
+use crate::Parameters;
+use ixa::{
+    context::Context,
+    create_report_trait,
+    global_properties::ContextGlobalPropertiesExt,
+    people::{ContextPeopleExt, PersonCreatedEvent},
+    report::{ContextReportExt, Report},
+};
+use serde::{Deserialize, Serialize};
+use std::path::Path;
+use std::path::PathBuf;
+
+#[derive(Serialize, Deserialize, Clone)]
+struct PersonReportItem {
+    time: f64,
+    person_id: String,
+    age_group: AgeGroupRisk,
+    property: String,
+    property_prev: String,
+    property_current: String,
+}
+
+create_report_trait!(PersonReportItem);
+
+fn handle_person_created(context: &mut Context, event: PersonCreatedEvent) {
+    let person = event.person_id;
+    let age_group_person = context.get_person_property(person, VaccineAgeGroup);
+    context.send_report(PersonReportItem {
+        time: context.get_current_time(),
+        person_id: format!("{person}"),
+        age_group: age_group_person,
+        property: "Created".to_string(),
+        property_prev: String::new(),
+        property_current: String::new(),
+    });
+}
+
+
+pub fn init(context: &mut Context) {
+    let parameters = context.get_global_property_value(Parameters)
+        .unwrap()
+        .clone();
+
+    let current_dir = Path::new(file!()).parent().unwrap();
+    context
+        .report_options()
+        .directory(PathBuf::from(current_dir));
+
+    context.add_report::<PersonReportItem>(&parameters.output_file);
+    context.subscribe_to_event(|context, event: PersonCreatedEvent| {
+        handle_person_created(context, event);
+    });
+}
