8a65a83dfed41da16adaba5a83d59af89757b1e4
Eric Rescorla <141454109+ekr-cfa@users.noreply.github.com>
2024-11-25 09:41:48 -0500

    Load global properties from a configuration file (#85)

--- src/global_properties.rs
+++ src/global_properties.rs
@@ -2,11 +2,59 @@ use crate::context::Context;
 use crate::error::IxaError;
 use serde::de::DeserializeOwned;
 use std::any::{Any, TypeId};
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::fs;
 use std::io::BufReader;
 use std::path::Path;
+use std::sync::Arc;
+use std::sync::LazyLock;
+use std::sync::Mutex;
+
+type PropertySetterFn =
+    dyn Fn(&mut Context, &str, serde_json::Value) -> Result<(), IxaError> + Send + Sync;
+
+#[allow(clippy::type_complexity)]
+// This is a global list of all the global properties that
+// are compiled in. Fundamentally it's a HashMap of property
+// names to the setter function, but it's wrapped in the
+// RefCell/Mutex/LazyLock combo to allow it to be globally
+// shared and initialized at startup time while still being
+// safe.
+pub static GLOBAL_PROPERTIES: LazyLock<Mutex<RefCell<HashMap<String, Arc<PropertySetterFn>>>>> =
+    LazyLock::new(|| Mutex::new(RefCell::new(HashMap::new())));
+
+#[allow(clippy::missing_panics_doc)]
+pub fn add_global_property<T: GlobalProperty>(name: &str)
+where
+    for<'de> <T as GlobalProperty>::Value: serde::Deserialize<'de>,
+{
+    let properties = GLOBAL_PROPERTIES.lock().unwrap();
+    properties.borrow_mut().insert(
+        name.to_string(),
+        Arc::new(
+            |context: &mut Context, name, value| -> Result<(), IxaError> {
+                let val: T::Value = serde_json::from_value(value)?;
+                if context.get_global_property_value(T::new()).is_some() {
+                    return Err(IxaError::IxaError(format!("Duplicate property {name}")));
+                }
+                context.set_global_property_value(T::new(), val);
+                Ok(())
+            },
+        ),
+    );
+}
+
+#[allow(clippy::missing_panics_doc)]
+fn get_global_property(name: &String) -> Option<Arc<PropertySetterFn>> {
+    let properties = GLOBAL_PROPERTIES.lock().unwrap();
+    let tmp = properties.borrow();
+    match tmp.get(name) {
+        Some(func) => Some(Arc::clone(func)),
+        None => None,
+    }
+}
 
 /// Defines a global property with the following parameters:
 /// * `$global_property`: Name for the identifier type of the global property
@@ -19,6 +67,21 @@ macro_rules! define_global_property {
 
         impl $crate::global_properties::GlobalProperty for $global_property {
             type Value = $value;
+
+            fn new() -> Self {
+                $global_property
+            }
+        }
+
+        paste::paste! {
+            #[ctor::ctor]
+            fn [<$global_property:snake _register>]() {
+                let module = module_path!();
+                let mut name = module.split("::").next().unwrap().to_string();
+                name += ".";
+                name += stringify!($global_property);
+                $crate::global_properties::add_global_property::<$global_property>(&name);
+            }
         }
     };
 }
@@ -27,6 +90,8 @@ macro_rules! define_global_property {
 /// in a global scope during the simulation, such as simulation parameters.
 pub trait GlobalProperty: Any {
     type Value: Any;
+
+    fn new() -> Self;
 }
 
 pub use define_global_property;
@@ -68,6 +133,25 @@ pub trait ContextGlobalPropertiesExt {
         &mut self,
         file_path: &Path,
     ) -> Result<T, IxaError>;
+
+    /// Load global properties from a JSON file.
+    ///
+    /// The expected structure is a dictionary with each name being
+    /// the name of the struct prefixed with the crate name, as in:
+    /// `ixa.NumFluVariants` and the value being an object which can
+    /// serde deserialize into the relevant struct.
+    ///
+    /// # Errors
+    /// Will return an `IxaError` if:
+    /// * The `file_path` doesn't exist
+    /// * The file isn't valid JSON
+    /// * A specified object doesn't correspond to an existing global property.
+    /// * There are two values for the same object.
+    ///
+    /// Ixa automatically knows about any property defined with
+    /// `define_global_property!()` so you don't need to register them
+    /// explicitly.
+    fn load_global_properties(&mut self, file_name: &Path) -> Result<(), IxaError>;
 }
 
 impl GlobalPropertiesDataContainer {
@@ -123,12 +207,29 @@ impl ContextGlobalPropertiesExt for Context {
         let config = serde_json::from_reader(reader)?;
         Ok(config)
     }
+
+    fn load_global_properties(&mut self, file_name: &Path) -> Result<(), IxaError> {
+        let config_file = fs::File::open(file_name)?;
+        let reader = BufReader::new(config_file);
+        let val: serde_json::Map<String, serde_json::Value> = serde_json::from_reader(reader)?;
+
+        for (k, v) in val {
+            if let Some(handler) = get_global_property(&k) {
+                handler(self, &k, v)?;
+            } else {
+                return Err(IxaError::from(format!("No global property: {k}")));
+            }
+        }
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     use crate::context::Context;
+    use crate::error::IxaError;
     use serde::{Deserialize, Serialize};
     use std::path::PathBuf;
     use tempfile::tempdir;
@@ -193,5 +294,70 @@ mod test {
             .clone();
         assert_eq!(params_read.days, params.days);
         assert_eq!(params_read.diseases, params.diseases);
+    }
+
+    #[derive(Deserialize)]
+    pub struct Property1Type {
+        field_int: u32,
+        field_str: String,
+    }
+    define_global_property!(Property1, Property1Type);
+
+    #[derive(Deserialize)]
+    pub struct Property2Type {
+        field_int: u32,
+    }
+    define_global_property!(Property2, Property2Type);
+
+    #[test]
+    fn read_global_properties() {
+        let mut context = Context::new();
+        let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("tests/data/global_properties_test1.json");
+        context.load_global_properties(&path).unwrap();
+        let p1 = context.get_global_property_value(Property1).unwrap();
+        assert_eq!(p1.field_int, 1);
+        assert_eq!(p1.field_str, "test");
+        let p2 = context.get_global_property_value(Property2).unwrap();
+        assert_eq!(p2.field_int, 2);
+    }
+
+    #[test]
+    fn read_unknown_property() {
+        let mut context = Context::new();
+        let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("tests/data/global_properties_missing.json");
+        match context.load_global_properties(&path) {
+            Err(IxaError::IxaError(msg)) => {
+                assert_eq!(msg, "No global property: ixa.Property3");
+            }
+            _ => panic!("Unexpected error type"),
+        }
+    }
+
+    #[test]
+    fn read_malformed_property() {
+        let mut context = Context::new();
+        let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("tests/data/global_properties_malformed.json");
+        let error = context.load_global_properties(&path);
+        println!("Error {error:?}");
+        match error {
+            Err(IxaError::JsonError(_)) => {}
+            _ => panic!("Unexpected error type"),
+        }
+    }
+
+    #[test]
+    fn read_duplicate_property() {
+        let mut context = Context::new();
+        let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("tests/data/global_properties_test1.json");
+        context.load_global_properties(&path).unwrap();
+        let error = context.load_global_properties(&path);
+        match error {
+            Err(IxaError::IxaError(_)) => {}
+            _ => panic!("Unexpected error type"),
+        }
     }
 }
