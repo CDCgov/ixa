42252a85654d670a5ddd00e3fa5e9fd9279a697a
Robert Jacobson <RJacobson@cdc.gov>
2025-01-15 16:42:29 -0600

    Made both `PersonId` and `PlandId` a tuple-style newtype instead of a single field struct and modified usages accordingly. Changed instances of `id` to `plan_id` or `person_id`.

--- examples/basic-infection/incidence_report.rs
+++ examples/basic-infection/incidence_report.rs
@@ -1,17 +1,17 @@
 use crate::infection_manager::InfectionStatusEvent;
 use crate::people::InfectionStatusValue;
 use ixa::context::Context;
-use ixa::create_report_trait;
 use ixa::error::IxaError;
 use ixa::report::ContextReportExt;
 use ixa::report::Report;
+use ixa::{create_report_trait, PersonId};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
 #[derive(Serialize, Deserialize, Clone)]
 struct IncidenceReportItem {
     time: f64,
-    person_id: String,
+    person_id: PersonId,
     infection_status: InfectionStatusValue,
 }
 
@@ -20,7 +20,7 @@ create_report_trait!(IncidenceReportItem);
 fn handle_infection_status_change(context: &mut Context, event: InfectionStatusEvent) {
     context.send_report(IncidenceReportItem {
         time: context.get_current_time(),
-        person_id: event.person_id.to_string(),
+        person_id: event.person_id,
         infection_status: event.current,
     });
 }
