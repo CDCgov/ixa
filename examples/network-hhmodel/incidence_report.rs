195cbb029fd383b520bea18865262e78e0451e43
Chirag Kumar <kzs9@cdc.gov>
2025-02-11 17:49:03 +0000

    fixing tests and cleaning up

--- examples/network-hhmodel/incidence_report.rs
+++ examples/network-hhmodel/incidence_report.rs
@@ -59,44 +59,22 @@ pub fn init(context: &mut Context) -> Result<(), IxaError> {
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
     use crate::parameters::ParametersValues;
     use crate::{incidence_report, loader, network, seir, MainRng, PersonId};
-    use csv::ReaderBuilder;
     use ixa::{context::Context, random::ContextRandomExt, ContextPeopleExt};
-    use std::error::Error;
     use std::path::Path;
-    use std::{fs, io};
 
-    fn ensure_directory_exists(path: &Path) -> io::Result<()> {
-        if !path.exists() {
-            fs::create_dir_all(path)?;
-        }
-        Ok(())
-    }
-
-    fn read_csv_column_names(path: &Path) -> Result<Vec<String>, Box<dyn Error>> {
-        use std::fs::File;
-        let file = File::open(path)?;
-        let mut rdr = ReaderBuilder::new().has_headers(true).from_reader(file);
-
+    fn read_csv_column_names(path: &Path) -> Result<Vec<String>, IxaError> {
+        let mut rdr = csv::Reader::from_path(path)?;
         let headers = rdr.headers()?;
         let column_names = headers.iter().map(|s| s.to_string()).collect();
         Ok(column_names)
     }
 
     #[test]
-    fn test_output_directory_created() -> std::io::Result<()> {
-        let path: &Path = Path::new("examples/network-hhmodel/tests");
-        ensure_directory_exists(&path)?;
-        assert!(path.exists());
-        assert!(path.is_dir());
-        Ok(())
-    }
-
-    #[test]
-    fn test_incidence_report() -> Result<(), Box<dyn Error>> {
+    fn test_incidence_report() {
         let parameters = ParametersValues {
             incubation_period: 8.0,
             infectious_period: 27.0,
@@ -107,35 +85,37 @@ mod tests {
             data_dir: "examples/network-hhmodel/tests".to_owned(),
             output_dir: "examples/network-hhmodel/tests".to_owned(),
         };
-        let mut context = Context::new();
+        // We need to put this code where we actually set up the model and write to the report in
+        // its own scope so that the report flushes at scope close, allowing us to read the values
+        // in it.
+        {
+            let mut context = Context::new();
 
-        context.init_random(2);
+            context.init_random(2);
 
-        context
-            .set_global_property_value(Parameters, parameters.clone())
-            .unwrap();
+            context
+                .set_global_property_value(Parameters, parameters.clone())
+                .unwrap();
 
-        let people = loader::init(&mut context);
-        network::init(&mut context, &people);
-        incidence_report::init(&mut context).unwrap();
-        let to_infect: Vec<PersonId> = vec![context.sample_person(MainRng, ()).unwrap()];
-        context.set_person_property(to_infect[0], InfectedBy, Some(to_infect[0]));
+            let people = loader::init(&mut context);
+            network::init(&mut context, &people);
+            incidence_report::init(&mut context).unwrap();
+            let to_infect: Vec<PersonId> = vec![context.sample_person(MainRng, ()).unwrap()];
+            context.set_person_property(to_infect[0], InfectedBy, Some(to_infect[0]));
 
-        #[allow(clippy::vec_init_then_push)]
-        seir::init(&mut context, &to_infect);
-        context.execute();
+            #[allow(clippy::vec_init_then_push)]
+            seir::init(&mut context, &to_infect);
+            context.execute();
+        }
+        let path = Path::new(&parameters.output_dir);
+        assert!(path.try_exists().unwrap());
+        let output_path = path.join("incidence.csv");
+        assert!(output_path.try_exists().unwrap());
 
-        let path: &Path = Path::new(&parameters.output_dir);
-        assert!(path.exists());
-        let data_path = format!("{}/incidence.csv", parameters.output_dir);
-        println!("{}", data_path);
-
-        let column_names = read_csv_column_names(Path::new(&data_path))?;
+        let column_names = read_csv_column_names(&output_path).unwrap();
         assert_eq!(
             column_names,
             vec!["time", "person_id", "infection_status", "infected_by"]
         );
-
-        Ok(())
     }
 }
