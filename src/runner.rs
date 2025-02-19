f17b9c97181daa6dc64c3ca7bdc61d755661cc3e
Gil Sharon <as81@cdc.gov>
2025-02-19 17:27:00 +0000

    added test

--- src/runner.rs
+++ src/runner.rs
@@ -327,4 +327,30 @@ mod tests {
         let result = run_with_args_internal(test_args, None, |_, _, _: Option<()>| Ok(()));
         assert!(result.is_ok());
     }
+
+    #[test]
+    fn test_run_with_logging_modules() {
+        let output = assert_cmd::Command::cargo_bin("runner_test_debug")
+            .unwrap()
+            .args([
+                "--debugger",
+                "1.0",
+                "--log-level",
+                "rustyline:Debug,ixa:Trace",
+            ])
+            .write_stdin("population\n")
+            .output();
+        match String::from_utf8(output.unwrap().stdout) {
+            Ok(s) => {
+                // Check if the output contains some of the expected log messages
+                assert!(s.contains("Logging enabled for rustyline at level DEBUG"));
+                assert!(s.contains("Logging enabled for ixa at level TRACE"));
+                assert!(s.contains("TRACE ixa::debugger - scheduling debugger"));
+            }
+            Err(e) => {
+                println!("Failed to convert: {e}");
+                panic!();
+            }
+        }
+    }
 }
