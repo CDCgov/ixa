dc71d2bb8fbd80f1bbea05003a21a592575e8e9f
k88hudson-cfa <ryl8@cdc.gov>
2025-02-24 11:55:20 -0500

    Sketch of ixa params derive macro

--- examples/parameter-loading/main.rs
+++ examples/parameter-loading/main.rs
@@ -1,11 +1,13 @@
 use ixa::error::IxaError;
+use ixa::global_properties::IxaParams;
 use ixa::people::ContextPeopleExt;
 use ixa::random::ContextRandomExt;
 use ixa::{
     context::Context, define_person_property_with_default,
     global_properties::ContextGlobalPropertiesExt,
 };
-use std::path::Path;
+use ixa_derive::IxaParams;
+use std::path::{Path, PathBuf};
 
 mod incidence_report;
 mod infection_manager;
@@ -14,6 +16,7 @@ mod transmission_manager;
 
 use crate::parameters_loader::Parameters;
 
+use clap::Parser;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Hash, Eq, PartialEq, Clone, Copy, Serialize, Deserialize)]
@@ -58,7 +61,31 @@ fn initialize() -> Result<Context, IxaError> {
     Ok(context)
 }
 
+#[derive(IxaParams, Debug, Serialize, Deserialize, Parser)]
+#[command(version, about)]
+pub struct Params {
+    /// The number of infections we seed the population with.
+    pub initial_infections: usize,
+    /// The maximum run time of the simulation; even if there are still infections
+    /// scheduled to occur, the simulation will stop at this time.
+    pub max_time: f64,
+    /// The random seed for the simulation.
+    pub seed: u64,
+    /// A constant rate of infection applied to all individuals.
+    pub rate_of_infection: f64,
+    /// The duration of the infection in days
+    pub infection_duration: f64,
+    /// The period at which to report tabulated values
+    pub report_period: f64,
+    /// The path to the synthetic population file loaded in `population_loader`
+    pub synth_population_file: PathBuf,
+}
+
 fn main() {
+    let test =
+        Params::parse_config_and_args(std::path::Path::new("examples/parameter-loading/ixa.json"))
+            .unwrap();
+    println!("{:?}", test);
     let mut context = initialize().expect("Could not initialize context.");
 
     context.execute();
