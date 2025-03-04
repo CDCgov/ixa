1d2ac56a3beb4c4c70e7d461f37a918495426d2c
Gil Sharon <as81@cdc.gov>
2025-03-04 15:23:56 -0500

    updated setup script

--- scripts/setup_new_ixa_project.sh
+++ scripts/setup_new_ixa_project.sh
@@ -6,6 +6,16 @@ if [ -n "$1" ]; then
     ixa_branch=$1
 fi
 
+# function to check if last shell command was successful, if not print input message and exit
+check_success() {
+    if [ $? -ne 0 ]; then
+        echo $1
+        exit
+    fi
+}
+
+echo "Setting up new ixa project with branch $ixa_branch"
+
 # check if cargo is installed
 if ! command -v cargo &> /dev/null
 then
@@ -23,17 +33,22 @@ fi
 cargo add --git "https://github.com/CDCgov/ixa" ixa --branch $ixa_branch
 
 # add .gitignore from ixa
-curl -o .gitignore https://raw.githubusercontent.com/CDCgov/ixa/$ixa_branch/.gitignore
+curl -s -f -o .gitignore --variable %B=$ixa_branch --expand-url "https://raw.githubusercontent.com/CDCgov/ixa/{{B:url}}/.gitignore"
+check_success "Failed to download .gitignore from ixa"
 
 # add the pre-commit hook from ixa
-curl -o .pre-commit-config.yaml https://raw.githubusercontent.com/CDCgov/ixa/$ixa_branch/.pre-commit-config.yaml
+curl -s -f -o .pre-commit-config.yaml --variable %B=$ixa_branch --expand-url https://raw.githubusercontent.com/CDCgov/ixa/{{B:url}}/.pre-commit-config.yaml
+check_success "Failed to download pre-commit-config.yaml from ixa"
 
 # add github action from ixa
 mkdir -p .github/workflows
-curl -o .github/workflows/build-test.yaml https://raw.githubusercontent.com/CDCgov/ixa/$ixa_branch/template/.github/workflows/build-test.yaml
-curl -o .github/workflows/pre-commit.yaml https://raw.githubusercontent.com/CDCgov/ixa/$ixa_branch/template/.github/workflows/pre-commit.yaml
+curl -s -f -o .github/workflows/build-test.yaml --variable %B=$ixa_branch --expand-url https://raw.githubusercontent.com/CDCgov/ixa/{{B:url}}/scripts/template/.github/workflows/build-test.yaml
+check_success "Failed to download build-test.yaml from ixa"
+curl -s -f -o .github/workflows/pre-commit.yaml --variable %B=$ixa_branch --expand-url https://raw.githubusercontent.com/CDCgov/ixa/{{B:url}}/scripts/template/.github/workflows/pre-commit.yaml
+check_success "Failed to download pre-commit.yaml from ixa"
 
 # override main.rs with ixa basic example
-curl -o src/main.rs https://raw.githubusercontent.com/CDCgov/ixa/$ixa_branch/examples/basic/main.rs
+curl -s -f -o src/main.rs --variable %B=$ixa_branch --expand-url https://raw.githubusercontent.com/CDCgov/ixa/{{B:url}}/examples/basic/main.rs
+check_success "Failed to download main.rs from ixa"
 
-echo "Project setup complete"
+echo "Project setup complete from branch $ixa_branch"
