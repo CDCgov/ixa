CDCgov/ixa

commit ee3c219bed26fd8251ea8644b5ecd263c0186023

Add an API to get properties for individual people (#184)

src/people/context_extension.rs
@@ -138,6 +138,7 @@ impl ContextPeopleExt for Context {
     ) -> T::Value {
         let data_container = self.get_data_container(PeoplePlugin)
             .expect("PeoplePlugin is not initialized; make sure you add a person before accessing properties");
+        self.register_property::<T>();
 
         if T::is_derived() {
             return T::compute(self, person_id);
@@ -162,6 +163,8 @@ impl ContextPeopleExt for Context {
         property: T,
         value: T::Value,
     ) {
+        self.register_property::<T>();
+
         assert!(!T::is_derived(), "Cannot set a derived property");
 
         // This function can be called in two separate modes:
@@ -247,7 +250,6 @@ impl ContextPeopleExt for Context {
         }
 
         self.register_property::<T>();
-        self.register_indexer::<T>();
 
         let data_container = self.get_data_container(PeoplePlugin).unwrap();
         let mut index = data_container
@@ -309,24 +311,32 @@ impl ContextPeopleExt for Context {
     }
 
     fn register_property<T: PersonProperty + 'static>(&self) {
-        let data_container = self.get_data_container(PeoplePlugin).unwrap();
-        if !data_container
+        let data_container = self.get_data_container(PeoplePlugin).
+            expect("PeoplePlugin is not initialized; make sure you add a person before accessing properties");
+        if data_container
             .registered_derived_properties
             .borrow()
             .contains(&TypeId::of::<T>())
         {
-            let instance = T::get_instance();
-            let dependencies = instance.non_derived_dependencies();
-            for dependency in dependencies {
-                let mut dependency_map = data_container.dependency_map.borrow_mut();
-                let derived_prop_list = dependency_map.entry(dependency).or_default();
-                derived_prop_list.push(Box::new(instance));
-            }
-            data_container
-                .registered_derived_properties
-                .borrow_mut()
-                .insert(TypeId::of::<T>());
+            return;
         }
+        let instance = T::get_instance();
+        let dependencies = instance.non_derived_dependencies();
+        for dependency in dependencies {
+            let mut dependency_map = data_container.dependency_map.borrow_mut();
+            let derived_prop_list = dependency_map.entry(dependency).or_default();
+            derived_prop_list.push(Box::new(instance));
+        }
+        data_container
+            .people_types
+            .borrow_mut()
+            .insert(T::name().to_string(), TypeId::of::<T>());
+        data_container
+            .registered_derived_properties
+            .borrow_mut()
+            .insert(TypeId::of::<T>());
+
+        self.register_indexer::<T>();
     }
 
     fn tabulate_person_properties<T: Tabulator, F>(&self, tabulator: &T, print_fn: F)

src/people/data.rs
@@ -32,6 +32,7 @@ pub(super) struct PeopleData {
     pub(super) registered_derived_properties: RefCell<HashSet<TypeId>>,
     pub(super) dependency_map: RefCell<HashMap<TypeId, Vec<Box<dyn PersonPropertyHolder>>>>,
     pub(super) property_indexes: RefCell<HashMap<TypeId, Index>>,
+    pub(super) people_types: RefCell<HashMap<String, TypeId>>,
 }
 
 // The purpose of this trait is to enable storing a Vec of different

src/people/mod.rs
@@ -71,12 +71,12 @@
 mod context_extension;
 mod data;
 mod event;
+pub(crate) mod external_api;
 mod index;
 mod property;
 mod query;
 
 use crate::{context::Context, define_data_plugin};
-
 pub use context_extension::ContextPeopleExt;
 use data::PeopleData;
 pub use data::PersonPropertyHolder;
@@ -105,7 +105,8 @@ define_data_plugin!(
         properties_map: RefCell::new(HashMap::new()),
         registered_derived_properties: RefCell::new(HashSet::new()),
         dependency_map: RefCell::new(HashMap::new()),
-        property_indexes: RefCell::new(HashMap::new())
+        property_indexes: RefCell::new(HashMap::new()),
+        people_types: RefCell::new(HashMap::new()),
     }
 );
 

src/external_api.rs
@@ -157,3 +157,98 @@ pub(crate) mod r#continue {
         }
     }
 }
+
+pub(crate) mod people {
+    use crate::people::{external_api::ContextPeopleExtCrate, ContextPeopleExt, PersonId};
+    use crate::Context;
+    use crate::IxaError;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Deserialize)]
+    pub(crate) enum ArgsEnum {
+        Get {
+            person_id: PersonId,
+            property: String,
+        },
+        Query {
+            properties: Vec<(String, String)>,
+        },
+    }
+
+    #[derive(Deserialize)]
+    pub(crate) enum Args {
+        People(ArgsEnum),
+    }
+
+    #[derive(Serialize, Debug, Eq, PartialEq)]
+    pub(crate) enum Retval {
+        Properties(Vec<(String, String)>),
+    }
+    pub(crate) struct Api {}
+
+    impl super::ExtApi for Api {
+        type Args = Args;
+        type Retval = Retval;
+
+        fn run(context: &mut Context, args: &Args) -> Result<Retval, IxaError> {
+            let Args::People(args) = args;
+
+            match args {
+                ArgsEnum::Get {
+                    person_id,
+                    property,
+                } => {
+                    if person_id.0 >= context.get_current_population() {
+                        return Err(IxaError::IxaError(format!("No person with id {person_id}")));
+                    }
+
+                    let value = context.get_person_property_by_name(property, *person_id)?;
+                    Ok(Retval::Properties(vec![(property.to_string(), value)]))
+                }
+                ArgsEnum::Query {
+                    properties: _global_properties,
+                } => {
+                    unimplemented!();
+                }
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+        use crate::external_api::run_ext_api;
+        use crate::Context;
+        #[test]
+        fn query_nonexistent_user() {
+            let mut context = Context::new();
+
+            let res = run_ext_api::<super::Api>(
+                &mut context,
+                &Args::People(ArgsEnum::Get {
+                    person_id: PersonId(0),
+                    property: String::from("abc"),
+                }),
+            );
+
+            println!("{res:?}");
+            assert!(matches!(res, Err(IxaError::IxaError(_))));
+        }
+
+        #[test]
+        fn query_nonexistent_property() {
+            let mut context = Context::new();
+            let _ = context.add_person(());
+            let res = run_ext_api::<super::Api>(
+                &mut context,
+                &Args::People(ArgsEnum::Get {
+                    person_id: PersonId(0),
+                    property: String::from("abc"),
+                }),
+            );
+
+            println!("{res:?}");
+            assert!(matches!(res, Err(IxaError::IxaError(_))));
+        }
+    }
+}

src/people/external_api.rs
@@ -1,0 +1,60 @@
+use crate::people::PeoplePlugin;
+use crate::Context;
+use crate::IxaError;
+use crate::PersonId;
+
+pub(crate) trait ContextPeopleExtCrate {
+    fn get_person_property_by_name(
+        &self,
+        name: &str,
+        person_id: PersonId,
+    ) -> Result<String, IxaError>;
+}
+
+impl ContextPeopleExtCrate for Context {
+    fn get_person_property_by_name(
+        &self,
+        name: &str,
+        person_id: PersonId,
+    ) -> Result<String, IxaError> {
+        let data_container = self.get_data_container(PeoplePlugin).unwrap();
+        let type_id = *data_container
+            .people_types
+            .borrow()
+            .get(name)
+            .ok_or(IxaError::IxaError(format!("No property '{name}'")))?;
+
+        let index = data_container.get_index_ref(type_id).unwrap(); // This should exist
+        Ok((index.get_display)(self, person_id))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::ContextPeopleExtCrate;
+    use crate::people::{define_person_property, ContextPeopleExt};
+    use crate::Context;
+    use crate::ContextRandomExt;
+
+    define_person_property!(Age, u8);
+
+    #[test]
+    fn get_property_string() {
+        let mut context = Context::new();
+        context.init_random(42);
+
+        let person1 = context.add_person((Age, 10)).unwrap();
+        let age = context.get_person_property_by_name("Age", person1).unwrap();
+        assert_eq!(age, "10");
+    }
+
+    #[test]
+    fn get_unknown_property_string() {
+        let mut context = Context::new();
+        context.init_random(42);
+
+        let person1 = context.add_person((Age, 10)).unwrap();
+        let age = context.get_person_property_by_name("Unknown", person1);
+        assert!(age.is_err());
+    }
+}

src/people/query.rs
@@ -1,4 +1,3 @@
-use crate::people::context_extension::ContextPeopleExtInternal;
 use crate::people::index::IndexValue;
 use crate::{Context, ContextPeopleExt, PersonProperty};
 use seq_macro::seq;
@@ -26,7 +25,6 @@ impl Query for () {
 impl<T1: PersonProperty + 'static> Query for (T1, T1::Value) {
     fn setup(context: &Context) {
         context.register_property::<T1>();
-        context.register_indexer::<T1>();
     }
 
     fn get_query(&self) -> Vec<(TypeId, IndexValue)> {
@@ -51,7 +49,6 @@ macro_rules! impl_query {
                 fn setup(context: &Context) {
                     #(
                         context.register_property::<T~N>();
-                        context.register_indexer::<T~N>();
                     )*
                 }
 

src/web_api.rs
@@ -1,7 +1,7 @@
 use crate::context::{run_with_plugin, Context};
 use crate::define_data_plugin;
 use crate::error::IxaError;
-use crate::external_api::{global_properties, next, population, run_ext_api, EmptyArgs};
+use crate::external_api::{global_properties, next, people, population, run_ext_api, EmptyArgs};
 use axum::extract::{Json, Path, State};
 use axum::{http::StatusCode, routing::post, Router};
 use rand::RngCore;
@@ -210,7 +210,7 @@ impl ContextWebApiExt for Context {
         );
         register_api_handler::<population::Api, EmptyArgs>(&mut api_data, "population");
         register_api_handler::<next::Api, next::Args>(&mut api_data, "next");
-
+        register_api_handler::<people::Api, people::Args>(&mut api_data, "people");
         // Record the data container.
         *data_container = Some(api_data);
 
@@ -229,6 +229,7 @@ impl ContextWebApiExt for Context {
 #[cfg(test)]
 mod tests {
     use super::ContextWebApiExt;
+    use crate::people::define_person_property;
     use crate::{define_global_property, ContextGlobalPropertiesExt};
     use crate::{Context, ContextPeopleExt};
     use reqwest::StatusCode;
@@ -237,7 +238,7 @@ mod tests {
     use std::thread;
 
     define_global_property!(WebApiTestGlobal, String);
-
+    define_person_property!(Age, u8);
     fn setup() -> (String, Context) {
         let mut context = Context::new();
         let url = context.setup_web_api(33339).unwrap();
@@ -245,8 +246,8 @@ mod tests {
         context
             .set_global_property_value(WebApiTestGlobal, "foobar".to_string())
             .unwrap();
-        context.add_person(()).unwrap();
-        context.add_person(()).unwrap();
+        context.add_person((Age, 1)).unwrap();
+        context.add_person((Age, 2)).unwrap();
         (url, context)
     }
 
@@ -270,8 +271,11 @@ mod tests {
             .json(req)
             .send()
             .unwrap();
-        assert_eq!(response.status(), StatusCode::OK);
-        response.json().unwrap()
+        let status = response.status();
+        let response = response.json().unwrap();
+        println!("{response:?}");
+        assert_eq!(status, StatusCode::OK);
+        response
     }
 
     // Send a request and check the response.
@@ -368,6 +372,25 @@ mod tests {
         );
         assert_eq!(res, json!({}));
 
+        let res = send_request(
+            &url,
+            "people",
+            &json!({
+                "People" : {
+                    "Get" : {
+                        "person_id": 0,
+                        "property" : "Age"
+                    }
+                }
+            }),
+        );
+        assert_eq!(
+            res,
+            json!({"Properties" : [
+                ( "Age",  "1" )
+            ]}
+            )
+        );
         // Valid JSON but wrong type.
         let res = send_request_text(
             &url,

commit f7d17f737bab26a44f83d68e5d9de6e12ce54051

fix: Fix double borrow during property registration
Fixes #301

A bug was introduced in ee3c219 that caused double borrows when
a query was run on a derived property whose dependencies had
not yet been initialized. This fixes the issue by ensuring
registration of dependencies happens first.

src/people/context_extension.rs
@@ -311,7 +311,7 @@ impl ContextPeopleExt for Context {
         let data_container = self.get_data_container(PeoplePlugin).
             expect("PeoplePlugin is not initialized; make sure you add a person before accessing properties");
         if data_container
-            .registered_derived_properties
+            .registered_properties
             .borrow()
             .contains(&TypeId::of::<T>())
         {
@@ -319,12 +319,22 @@ impl ContextPeopleExt for Context {
         }
 
         let instance = T::get_instance();
-        let dependencies = instance.non_derived_dependencies();
-        for dependency in dependencies {
-            let mut dependency_map = data_container.dependency_map.borrow_mut();
-            let derived_prop_list = dependency_map.entry(dependency).or_default();
-            derived_prop_list.push(Box::new(instance));
+
+        // In order to avoid borrowing recursively, we must register dependencies first.
+        if instance.is_derived() {
+            T::register_dependencies(self);
+
+            let dependencies = instance.non_derived_dependencies();
+            for dependency in dependencies {
+                let mut dependency_map = data_container.dependency_map.borrow_mut();
+                let derived_prop_list = dependency_map.entry(dependency).or_default();
+                derived_prop_list.push(Box::new(instance));
+            }
         }
+
+        // TODO<ryl8@cdc.gov>: We create an index for every property in order to
+        // support the get_person_property_by_name() function used in external_api.rs,
+        // but ideally this shouldn't be necessary.
         data_container
             .methods
             .borrow_mut()
@@ -334,7 +344,7 @@ impl ContextPeopleExt for Context {
             .borrow_mut()
             .insert(T::name().to_string(), TypeId::of::<T>());
         data_container
-            .registered_derived_properties
+            .registered_properties
             .borrow_mut()
             .insert(TypeId::of::<T>());
 
@@ -978,5 +988,40 @@ mod tests {
         assert!(count_p1 >= 8700);
         assert!(count_p2 >= 8700);
         assert!(count_p3 >= 8700);
+    }
+
+    mod property_initialization_queries {
+        use super::*;
+
+        define_rng!(PropertyInitRng);
+        define_person_property_with_default!(SimplePropWithDefault, u8, 1);
+        define_derived_property!(DerivedOnce, u8, [SimplePropWithDefault], |n| n * 2);
+        define_derived_property!(DerivedTwice, bool, [DerivedOnce], |n| n == 2);
+
+        #[test]
+        fn test_query_derived_property_not_initialized() {
+            let mut context = Context::new();
+            context.init_random(42);
+            let person = context.add_person(()).unwrap();
+            assert_eq!(
+                context
+                    .sample_person(PropertyInitRng, (DerivedOnce, 2))
+                    .unwrap(),
+                person
+            );
+        }
+
+        #[test]
+        fn test_query_derived_property_not_initialized_two_levels() {
+            let mut context = Context::new();
+            context.init_random(42);
+            let person = context.add_person(()).unwrap();
+            assert_eq!(
+                context
+                    .sample_person(PropertyInitRng, (DerivedTwice, true))
+                    .unwrap(),
+                person
+            );
+        }
     }
 }

src/people/data.rs
@@ -31,7 +31,7 @@ pub(super) struct PeopleData {
     pub(super) current_population: usize,
     pub(super) methods: RefCell<HashMap<TypeId, Methods>>,
     pub(super) properties_map: RefCell<HashMap<TypeId, StoredPeopleProperties>>,
-    pub(super) registered_derived_properties: RefCell<HashSet<TypeId>>,
+    pub(super) registered_properties: RefCell<HashSet<TypeId>>,
     pub(super) dependency_map: RefCell<HashMap<TypeId, Vec<Box<dyn PersonPropertyHolder>>>>,
     pub(super) property_indexes: RefCell<HashMap<TypeId, Index>>,
     pub(super) people_types: RefCell<HashMap<String, TypeId>>,

src/people/mod.rs
@@ -103,7 +103,7 @@ define_data_plugin!(
         current_population: 0,
         methods: RefCell::new(HashMap::new()),
         properties_map: RefCell::new(HashMap::new()),
-        registered_derived_properties: RefCell::new(HashSet::new()),
+        registered_properties: RefCell::new(HashSet::new()),
         dependency_map: RefCell::new(HashMap::new()),
         property_indexes: RefCell::new(HashMap::new()),
         people_types: RefCell::new(HashMap::new()),

src/people/property.rs
@@ -23,6 +23,9 @@ pub trait PersonProperty: Copy {
     fn dependencies() -> Vec<Box<dyn PersonPropertyHolder>> {
         panic!("Dependencies not implemented");
     }
+    fn register_dependencies(_: &Context) {
+        panic!("Dependencies not implemented");
+    }
     fn compute(context: &Context, person_id: PersonId) -> Self::Value;
     fn get_instance() -> Self;
     fn name() -> &'static str;
@@ -130,6 +133,9 @@ macro_rules! define_derived_property {
             fn is_derived() -> bool { true }
             fn dependencies() -> Vec<Box<dyn $crate::people::PersonPropertyHolder>> {
                 vec![$(Box::new($dependency)),+]
+            }
+            fn register_dependencies(context: &$crate::context::Context) {
+                $(context.register_property::<$dependency>();)+
             }
             fn get_instance() -> Self {
                 $derived_property