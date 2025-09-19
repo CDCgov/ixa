24166caa3c3d56532fad0ff24cc85277dcc41da6
Robert Jacobson <RJacobson@cdc.gov>
2025-09-18 21:59:52 -0500

    Added `index_benches.rs`.

---
+++ ixa-bench/criterion/index_benches.rs
@@ -0,0 +1,109 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use ixa::prelude::*;
+use ixa::{define_multi_property, PersonId};
+use std::hint::black_box;
+
+define_rng!(IndexBenchRng);
+
+define_person_property!(Property10, u8, |context: &Context, _person: PersonId| {
+    context.sample_range(IndexBenchRng, 0..10)
+});
+define_person_property!(Property100, u8, |context: &Context, _person: PersonId| {
+    context.sample_range(IndexBenchRng, 0..100)
+});
+
+define_person_property!(MProperty10, u8, |context: &Context, _person: PersonId| {
+    context.sample_range(IndexBenchRng, 0..10)
+});
+define_person_property!(MProperty100, u8, |context: &Context, _person: PersonId| {
+    context.sample_range(IndexBenchRng, 0..100)
+});
+
+define_multi_property!(MProperty, (MProperty10, MProperty100));
+
+pub fn criterion_benchmark(criterion: &mut Criterion) {
+    let mut criterion = criterion.benchmark_group("indexing");
+
+    let mut context = Context::new();
+    for _ in 0..100000 {
+        let _ = context.add_person(());
+    }
+
+    let mut numbers: Vec<u8> = Vec::with_capacity(1000);
+    for _ in 0..1000 {
+        numbers.push(context.sample_range(IndexBenchRng, 0..100));
+    }
+
+    context.index_property(Property10);
+    context.index_property(Property100);
+    context.index_property(MProperty);
+
+    criterion.bench_function("query_people_count_single_indexed_property", |bencher| {
+        bencher.iter(|| {
+            for number in &numbers {
+                black_box(context.query_people_count(black_box((Property10, number % 10))));
+            }
+        });
+    });
+
+    criterion.bench_function(
+        "query_people_count_multiple_individually_indexed_properties",
+        |bencher| {
+            bencher.iter(|| {
+                for number in &numbers {
+                    black_box(context.query_people_count(black_box((
+                        (Property10, number * 3 % 10),
+                        (Property100, *number),
+                    ))));
+                }
+            });
+        },
+    );
+
+    criterion.bench_function("query_people_count_indexed_multi-property", |bencher| {
+        bencher.iter(|| {
+            for number in &numbers {
+                black_box(
+                    context.query_people_count(black_box((MProperty, (number * 3 % 10, *number)))),
+                );
+            }
+        });
+    });
+
+    criterion.bench_function("query_people_single_indexed_property", |bencher| {
+        bencher.iter(|| {
+            for number in &numbers {
+                black_box(context.query_people(black_box((Property10, number % 10))));
+            }
+        });
+    });
+
+    criterion.bench_function(
+        "query_people_multiple_individually_indexed_properties",
+        |bencher| {
+            bencher.iter(|| {
+                for number in &numbers {
+                    black_box(context.query_people(black_box((
+                        (Property10, number * 3 % 10),
+                        (Property100, *number),
+                    ))));
+                }
+            });
+        },
+    );
+
+    criterion.bench_function("query_people_indexed_multi-property", |bencher| {
+        bencher.iter(|| {
+            for number in &numbers {
+                black_box(
+                    context.query_people(black_box((MProperty, (number * 3 % 10, *number)))),
+                );
+            }
+        });
+    });
+
+    criterion.finish();
+}
+
+criterion_group!(index_benches, criterion_benchmark);
+criterion_main!(index_benches);
