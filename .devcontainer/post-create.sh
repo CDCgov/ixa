e8537487b3272d40e6ddaf111c6bafc05a5b42c0
k88hudson-cfa <ryl8@cdc.gov>
2025-09-30 14:31:20 -0400

    dont block on install

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,8 @@
 cd /workspaces/ixa
 
-# Install everything listed under the [tools] section of mise.toml
-mise trust
-mise install
+# Copy the custom welcome message
+# See https://github.com/orgs/community/discussions/43534
+sudo cp .devcontainer/terminal-welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 
 # See https://mise.jdx.dev/cli/activate.html
 # This makes sure that tools installed with mise (e.g., hyperfine, mdbook)
@@ -10,9 +10,10 @@ mise install
 echo 'eval "$(/usr/local/bin/mise activate bash)"' >> ~/.bashrc
 source ~/.bashrc
 
+# Install everything listed under the [tools] section of mise.toml
+mise trust
+mise install
+
 # Add pre-commit hooks
 mise install:hooks
 
-# Copy the custom welcome message
-# See https://github.com/orgs/community/discussions/43534
-sudo cp .devcontainer/terminal-welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
