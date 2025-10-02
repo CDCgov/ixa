b164f1976b70cefa9184e880589c151c3675039a
Gil Sharon <as81@cdc.gov>
2025-10-02 14:47:20 -0400

    test 5

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,9 +5,9 @@ cd /workspaces/ixa
 sudo cp .devcontainer/terminal-welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 
 sudo chown vscode:rustlang "${CARGO_HOME}"
-sudo chmod 775 "${CARGO_HOME}"
+sudo chmod -R 775 "${CARGO_HOME}"
 sudo chown vscode:rustlang "${RUSTUP_HOME}"
-sudo chmod 775 "${RUSTUP_HOME}"
+sudo chmod -R 775 "${RUSTUP_HOME}"
 
 # See https://mise.jdx.dev/cli/activate.html
 # This makes sure that tools installed with mise (e.g., hyperfine, mdbook)
@@ -19,5 +19,5 @@ source ~/.bashrc
 mise trust
 
 # Add pre-commit hooks
-#mise install:hooks
+mise install:hooks
 
