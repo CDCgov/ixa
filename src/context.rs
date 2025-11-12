46330dd845ee7efb6648bff67ce685d92e978c8a
Robert Jacobson <RJacobson@cdc.gov>
2025-11-11 23:51:39 -0600

    Accessor methods for property values on `Context`. Fixed `define_property` macros so that `Constant` initialization is used if a default constant is provided.

--- src/context.rs
+++ src/context.rs
@@ -21,6 +21,7 @@ use crate::{debugger::enter_debugger, plan::PlanSchedule};
 use crate::{get_data_plugin_count, trace, warn, ContextPeopleExt, HashMap, HashMapExt};
 use crate::entity::{Entity, EntityId};
 use crate::entity::entity_store::EntityStore;
+use crate::entity::property::{Property, PropertyInitializationKind};
 use crate::entity::property_list::PropertyList;
 use crate::entity::property_store::PropertyStore;
 
@@ -132,7 +133,6 @@ impl Context {
         }
     }
 
-
     pub fn add_entity<E: Entity, PL: PropertyList<E>>(&mut self, property_list: PL) -> EntityId<E> {
         // Check that the properties in the list are distinct.
         if let Err(msg) = PL::validate() {
@@ -151,6 +151,36 @@ impl Context {
         property_list.set_values_for_entity(new_entity_id.clone(), &self.property_store);
 
         new_entity_id
+    }
+
+    pub fn get_property<E: Entity, P: Property<E>>(&self, entity_id: EntityId<E>) -> P {
+        // ToDo(RobertJacobsonCDC): An alternative to the following is to always assume
+        //       that `None` means "not set" for "explicit" properties, that is, assume
+        //       that `get` is infallible for properties with a default constant. We
+        //       take a more conservative approach here and check for internal errors.
+        match P::initialization_kind() {
+            PropertyInitializationKind::Explicit => {
+                let property_store = self.property_store.get::<E, P>();
+                // A user error can cause this unwrap to fail.
+                property_store.get(entity_id).expect("attempted to get a property value with \"explicit\" initialization that was not set")
+            }
+
+            PropertyInitializationKind::Derived => {
+                P::compute_derived(self, entity_id)
+            }
+
+            PropertyInitializationKind::Constant => {
+                let property_store = self.property_store.get::<E, P>();
+                // If this unwrap fails, it is an internal ixa error, not a user error.
+                property_store.get(entity_id).expect("getting a property value with \"constant\" initialization should never fail")
+            }
+        }
+    }
+
+    pub fn set_property<E: Entity, P: Property<E>>(&self, entity_id: EntityId<E>, property_value: P) {
+        debug_assert!(P::initialization_kind() != PropertyInitializationKind::Derived, "cannot set a derived property");
+        let property_value_store = self.property_store.get::<E, P>();
+        property_value_store.set(entity_id, property_value);
     }
 
     /// Schedule the simulation to pause at time t and start the debugger.
@@ -660,10 +690,109 @@ mod tests {
 
     use ixa_derive::IxaEvent;
 
+    use crate::{define_entity, define_property, define_data_plugin};
     use super::*;
-    use crate::define_data_plugin;
 
     define_data_plugin!(ComponentA, Vec<u32>, vec![]);
+
+    define_entity!(Person);
+
+    define_property!(struct Age(u8), Person, is_required = true);
+
+    define_property!(
+        enum InfectionStatus {
+            Susceptible,
+            Infected,
+            Recovered,
+        },
+        Person,
+        default_const = InfectionStatus::Susceptible
+    );
+
+    define_property!(
+        struct Vaccinated(bool),
+        Person,
+        default_const = Vaccinated(false)
+    );
+
+    #[test]
+    fn add_an_entity() {
+        let mut context = Context::new();
+        let person = context.add_entity((
+            Age(12),
+            InfectionStatus::Susceptible,
+            Vaccinated(true),
+        ));
+        println!("{:?}", person);
+
+        let person = context.add_entity((
+            Age(34),
+            Vaccinated(true),
+        ));
+        println!("{:?}", person);
+
+        // Age is the only required property
+        let person = context.add_entity((
+            Age(120),
+        ));
+        println!("{:?}", person);
+    }
+
+    #[test]
+    #[should_panic(expected = "initialization list is missing required properties")]
+    fn add_an_entity_without_required_properties(){
+        let mut context = Context::new();
+        let person1 = context.add_entity((
+            InfectionStatus::Susceptible,
+            Vaccinated(true),
+        ));
+        println!("{:?}", person1);
+    }
+
+    #[test]
+    fn get_and_set_property_explicit() {
+        let mut context = Context::new();
+
+        // Create a person with required Age property
+        let person = context.add_entity((Age(25),));
+
+        // Retrieve it
+        let age: Age = context.get_property(person);
+        assert_eq!(age, Age(25));
+
+        // Change it
+        context.set_property(person, Age(26));
+        let age: Age = context.get_property(person);
+        assert_eq!(age, Age(26));
+    }
+
+    #[test]
+    fn get_property_with_constant_default() {
+        let mut context = Context::new();
+
+        // `Vaccinated` has a default value (false)
+        let person = context.add_entity((Age(40),));
+
+        // Even though we didn't set Vaccinated, it should exist with its default
+        let vaccinated: Vaccinated = context.get_property(person);
+        assert_eq!(vaccinated, Vaccinated(false));
+
+
+        // Now override
+        context.set_property(person, Vaccinated(true));
+        let vaccinated: Vaccinated = context.get_property(person);
+        assert_eq!(vaccinated, Vaccinated(true));
+    }
+
+    #[test]
+    fn get_property_with_enum_default() {
+        let mut context = Context::new();
+
+        // InfectionStatus has a default of Susceptible
+        let person = context.add_entity((Age(22),));
+        let status: InfectionStatus = context.get_property(person);
+        assert_eq!(status, InfectionStatus::Susceptible);
+    }
 
     #[test]
     fn empty_context() {
