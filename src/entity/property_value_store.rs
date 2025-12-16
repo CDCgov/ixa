d16052b7b63cbcca0d7443869de50a0c602ed9e5
Robert Jacobson <RJacobson@cdc.gov>
2025-12-16 11:33:41 -0600

    - Further type-erased `PropertyValueStore`, erased `Entity` type. - Put index in `RefCell` so it can be mutated in an immutable context (during `set_property`)

--- src/entity/property_value_store.rs
+++ src/entity/property_value_store.rs
@@ -11,7 +11,7 @@ Responsibilities:
 */
 
 use std::any::Any;
-
+use std::cell::{Ref, RefCell};
 use log::{error, trace};
 
 use crate::{
@@ -29,65 +29,81 @@ use crate::{
 use crate::entity::events::{PartialPropertyChangeEvent, PartialPropertyChangeEventCore};
 
 /// The `PropertyValueStore` trait defines the type-erased interface to the concrete property value storage.
-pub trait PropertyValueStore<E: Entity> {
-    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
+pub(crate) trait PropertyValueStore: Any {
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+    fn as_any(&self) -> &dyn Any;
 
     // Methods related to updating a value of a dependency
     /// Fetch the existing value of the property for the given `entity_id`, remove the `entity_id`
     /// from the corresponding index bucket, and return a `PartialPropertyChangeEvent` object
     /// wrapping the previous value and `entity_id` that can be used to emit a property change event.
-    fn create_partial_property_change(&mut self, entity_id: EntityId<E>) -> Box<dyn PartialPropertyChangeEvent>;
+    fn create_partial_property_change(&self, entity_id: usize, context: &Context) -> Box<dyn PartialPropertyChangeEvent>;
 
     // Index-related methods. Anything beyond these requires the `PropertyValueStoreCore<E, P>`.
-    fn add_entity_to_index_with_hash(&mut self, hash: HashValueType, entity_id: EntityId<E>);
-    fn remove_entity_from_index_with_hash(&mut self, hash: HashValueType, entity_id: EntityId<E>);
-    fn get_index_set_with_hash(&self, hash: HashValueType) -> Option<&HashSet<EntityId<E>>>;
+    fn add_entity_to_index_with_hash(&mut self, hash: HashValueType, entity_id: usize);
+    fn remove_entity_from_index_with_hash(&mut self, hash: HashValueType, entity_id: usize);
+    // fn get_index_set_with_hash(&self, hash: HashValueType) -> Option<Ref<HashSet<EntityId<E>>>>;
     fn is_indexed(&self) -> bool;
     fn set_indexed(&mut self, is_indexed: bool);
     fn index_unindexed_entities(&mut self, context: &Context);
 }
 
-impl<E: Entity, P: Property<E>> PropertyValueStore<E> for PropertyValueStoreCore<E, P> {
+impl<E: Entity, P: Property<E>> PropertyValueStore for PropertyValueStoreCore<E, P> {
     fn as_any_mut(&mut self) -> &mut dyn Any {
         self
     }
 
-    fn create_partial_property_change(&mut self, entity_id: EntityId<E>) -> Box<dyn PartialPropertyChangeEvent> {
-        // 1. Fetch the existing value of the property for the given `entity_id`
-        // 2. Remove the `entity_id` from the corresponding index bucket
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn create_partial_property_change(&self, entity_id: usize, context: &Context) -> Box<dyn PartialPropertyChangeEvent> {
+        // 1. Compute the existing value of the property for the given `entity_id`
+        // 2. Remove the `entity_id` from the corresponding index bucket (if its indexed)
         // 3. Return a `PartialPropertyChangeEvent` object wrapping the previous value and `entity_id`.
-        // ToDo(RobertJacobsonCDC): Is this always `Some`? Is this unwrap justified?
-        let previous_value = self.get(entity_id).unwrap();
-        if let Some(index) = &mut self.index {
+
+        // The entity_id has been type-erased but is guaranteed by the caller to be an `EntityId<E>`.
+        let entity_id = EntityId::<E>::new(entity_id);
+
+        let previous_value = P::compute_derived(context, entity_id);
+        if let Some(index) = &self.index {
+            let mut index = index.borrow_mut();
             index.remove_entity(&previous_value.make_canonical(), entity_id);
         }
         Box::new(PartialPropertyChangeEventCore::<E, P>::new(entity_id, previous_value))
     }
 
-    fn add_entity_to_index_with_hash(&mut self, hash: HashValueType, entity_id: EntityId<E>) {
+    fn add_entity_to_index_with_hash(&mut self, hash: HashValueType, entity_id: usize) {
+        // The entity_id has been type-erased but is guaranteed by the caller to be an `EntityId<E>`.
+        let entity_id = EntityId::<E>::new(entity_id);
+
         if let Some(index) = &mut self.index {
-            index.add_entity_with_hash(hash, entity_id);
+            index.get_mut().add_entity_with_hash(hash, entity_id);
         } else {
             error!("attempted to add an entity to an index for an unindexed property");
         }
     }
 
-    fn remove_entity_from_index_with_hash(&mut self, hash: HashValueType, entity_id: EntityId<E>) {
+    fn remove_entity_from_index_with_hash(&mut self, hash: HashValueType, entity_id: usize) {
+        // The entity_id has been type-erased but is guaranteed by the caller to be an `EntityId<E>`.
+        let entity_id = EntityId::<E>::new(entity_id);
+
         if let Some(index) = &mut self.index {
-            index.remove_entity_with_hash(hash, entity_id);
+            index.get_mut().remove_entity_with_hash(hash, entity_id);
         } else {
             error!("attempted to remove an entity from an index for an unindexed property");
         }
     }
 
-    fn get_index_set_with_hash(&self, hash: HashValueType) -> Option<&HashSet<EntityId<E>>> {
-        if let Some(index) = &self.index {
-            index.get_with_hash(hash)
-        } else {
-            error!("attempted to add an entity to a property index for an unindexed property");
-            None
-        }
-    }
+    // fn get_index_set_with_hash(&self, hash: HashValueType) -> Option<Ref<HashSet<EntityId<E>>>> {
+    //     if let Some(index) = &self.index {
+    //         let index = index.borrow();
+    //         Ref::filter_map(index, |idx| idx.get_with_hash(hash)).ok()
+    //     } else {
+    //         error!("attempted to add an entity to a property index for an unindexed property");
+    //         None
+    //     }
+    // }
 
     fn is_indexed(&self) -> bool {
         self.index.is_some()
@@ -95,7 +111,7 @@ impl<E: Entity, P: Property<E>> PropertyValueStore<E> for PropertyValueStoreCore
 
     fn set_indexed(&mut self, is_indexed: bool) {
         if is_indexed && !self.is_indexed() {
-            self.index = Some(Index::new());
+            self.index = Some( RefCell::new(Index::new()) );
         } else if !is_indexed {
             self.index = None;
         }
@@ -103,6 +119,7 @@ impl<E: Entity, P: Property<E>> PropertyValueStore<E> for PropertyValueStoreCore
 
     fn index_unindexed_entities(&mut self, context: &Context) {
         if let Some(index) = &mut self.index {
+            let index = index.get_mut();
             let current_pop = context.get_entity_count::<E>();
             trace!(
                 "{}: indexing unindexed entity {}..<{}",
