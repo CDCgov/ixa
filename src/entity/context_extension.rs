17d3ad1023ceabe1dcc9d6e3c8635d8f3b7e69b1
Robert Jacobson <RJacobson@cdc.gov>
2026-01-31 23:53:02 -0600

    chore: Additional unit tests. (#692)

--- src/entity/context_extension.rs
+++ src/entity/context_extension.rs
@@ -325,10 +325,15 @@ impl ContextEntitiesExt for Context {
 
 #[cfg(test)]
 mod tests {
-    use std::cell::Ref;
+    use std::cell::{Ref, RefCell};
+    use std::rc::Rc;
 
     use super::*;
+    use crate::prelude::PropertyChangeEvent;
     use crate::{define_derived_property, define_entity, define_multi_property, define_property};
+
+    define_entity!(Animal);
+    define_property!(struct Legs(u8), Animal, default_const = Legs(4));
 
     define_entity!(Person);
 
@@ -369,72 +374,300 @@ mod tests {
         }
     );
 
-    #[test]
-    fn add_an_entity() {
-        let mut context = Context::new();
-        let person = context.add_entity((Age(12), InfectionStatus::Susceptible, Vaccinated(true)));
-        println!("{:?}", person);
+    define_derived_property!(
+        enum RiskLevel {
+            Low,
+            Medium,
+            High,
+        },
+        Person,
+        [AgeGroup, Vaccinated, InfectionStatus],
+        |age_group, vaccinated, infection_status| {
+            match (age_group, vaccinated, infection_status) {
+                (AgeGroup::Senior, Vaccinated(false), InfectionStatus::Susceptible) => {
+                    RiskLevel::High
+                }
+                (_, Vaccinated(false), InfectionStatus::Susceptible) => RiskLevel::Medium,
+                _ => RiskLevel::Low,
+            }
+        }
+    );
 
-        let person = context.add_entity((Age(34), Vaccinated(true)));
-        println!("{:?}", person);
+    // ToDo(RobertJacobsonCDC): Enable this once #691 is resolved, https://github.com/CDCgov/ixa/issues/691.
+    // define_global_property!(GlobalDummy, u8);
+    // define_derived_property!(
+    //     struct MyDerivedProperty(u8),
+    //     Person,
+    //     [Age],
+    //     [GlobalDummy],
+    //     |age, global_dummy| {
+    //         MyDerivedProperty(age.0 + global_dummy)
+    //     }
+    // );
+
+    // Derived properties in a diamond dependency relationship
+    define_property!(struct IsRunner(bool), Person, default_const = IsRunner(false));
+    define_property!(struct IsSwimmer(bool), Person, default_const = IsSwimmer(false));
+    define_derived_property!(
+        struct AdultRunner(bool),
+        Person,
+        [AgeGroup, IsRunner],
+        | age_group, is_runner | {
+            AdultRunner(
+                age_group == AgeGroup::Adult
+                && is_runner.0
+            )
+        }
+    );
+    define_derived_property!(
+        struct AdultSwimmer(bool),
+        Person,
+        [AgeGroup, IsSwimmer],
+        | age_group, is_swimmer | {
+            AdultSwimmer(
+                age_group == AgeGroup::Adult
+                && is_swimmer.0
+            )
+        }
+    );
+    define_derived_property!(
+        struct AdultAthlete(bool),
+        Person,
+        [AdultSwimmer, AdultRunner],
+        | adult_swimmer, adult_runner | {
+            AdultAthlete(
+                adult_swimmer.0 || adult_runner.0
+            )
+        }
+    );
+
+    #[test]
+    fn add_and_count_entities() {
+        let mut context = Context::new();
+
+        let _person1 = context
+            .add_entity((Age(12), InfectionStatus::Susceptible, Vaccinated(true)))
+            .unwrap();
+        assert_eq!(context.get_entity_count::<Person>(), 1);
+
+        let _person2 = context.add_entity((Age(34), Vaccinated(true))).unwrap();
+        assert_eq!(context.get_entity_count::<Person>(), 2);
 
         // Age is the only required property
-        let person = context.add_entity((Age(120),));
-        println!("{:?}", person);
+        let _person3 = context.add_entity((Age(120),)).unwrap();
+        assert_eq!(context.get_entity_count::<Person>(), 3);
     }
 
     #[test]
-    #[should_panic(expected = "initialization list is missing required properties")]
     fn add_an_entity_without_required_properties() {
         let mut context = Context::new();
-        let person1 = context
-            .add_entity((InfectionStatus::Susceptible, Vaccinated(true)))
-            .unwrap();
-        println!("{:?}", person1);
+        let result = context.add_entity((InfectionStatus::Susceptible, Vaccinated(true)));
+
+        assert_eq!(
+            result,
+            Err("initialization list is missing required properties".to_string())
+        );
+    }
+
+    #[test]
+    fn new_entities_have_default_values() {
+        let mut context = Context::new();
+
+        // Create a person with required Age property
+        let person = context.add_entity((Age(25),)).unwrap();
+
+        // Retrieve and check their values
+        let age: Age = context.get_property(person);
+        assert_eq!(age, Age(25));
+        let infection_status: InfectionStatus = context.get_property(person);
+        assert_eq!(infection_status, InfectionStatus::Susceptible);
+        let vaccinated: Vaccinated = context.get_property(person);
+        assert_eq!(vaccinated, Vaccinated(false));
+
+        // Change them
+        context.set_property(person, Age(26));
+        context.set_property(person, InfectionStatus::Infected);
+        context.set_property(person, Vaccinated(true));
+
+        // Retrieve and check their values
+        let age: Age = context.get_property(person);
+        assert_eq!(age, Age(26));
+        let infection_status: InfectionStatus = context.get_property(person);
+        assert_eq!(infection_status, InfectionStatus::Infected);
+        let vaccinated: Vaccinated = context.get_property(person);
+        assert_eq!(vaccinated, Vaccinated(true));
     }
 
     #[test]
     fn get_and_set_property_explicit() {
         let mut context = Context::new();
 
-        // Create a person with required Age property
-        let person = context.add_entity((Age(25),)).unwrap();
+        // Create a person with explicit property values
+        let person = context
+            .add_entity((Age(25), InfectionStatus::Recovered, Vaccinated(true)))
+            .unwrap();
 
-        // Retrieve it
+        // Retrieve and check their values
         let age: Age = context.get_property(person);
         assert_eq!(age, Age(25));
-
-        // Change it
-        context.set_property(person, Age(26));
-        let age: Age = context.get_property(person);
-        assert_eq!(age, Age(26));
-    }
-
-    #[test]
-    fn get_property_with_constant_default() {
-        let mut context = Context::new();
-
-        // `Vaccinated` has a default value (false)
-        let person = context.add_entity((Age(40),)).unwrap();
-
-        // Even though we didn't set Vaccinated, it should exist with its default
-        let vaccinated: Vaccinated = context.get_property(person);
-        assert_eq!(vaccinated, Vaccinated(false));
-
-        // Now override
-        context.set_property(person, Vaccinated(true));
+        let infection_status: InfectionStatus = context.get_property(person);
+        assert_eq!(infection_status, InfectionStatus::Recovered);
         let vaccinated: Vaccinated = context.get_property(person);
         assert_eq!(vaccinated, Vaccinated(true));
+
+        // Change them
+        context.set_property(person, Age(26));
+        context.set_property(person, InfectionStatus::Infected);
+        context.set_property(person, Vaccinated(false));
+
+        // Retrieve and check their values
+        let age: Age = context.get_property(person);
+        assert_eq!(age, Age(26));
+        let infection_status: InfectionStatus = context.get_property(person);
+        assert_eq!(infection_status, InfectionStatus::Infected);
+        let vaccinated: Vaccinated = context.get_property(person);
+        assert_eq!(vaccinated, Vaccinated(false));
     }
 
     #[test]
-    fn get_property_with_enum_default() {
+    fn count_entities() {
         let mut context = Context::new();
 
-        // InfectionStatus has a default of Susceptible
-        let person = context.add_entity((Age(22),)).unwrap();
-        let status: InfectionStatus = context.get_property(person);
-        assert_eq!(status, InfectionStatus::Susceptible);
+        assert_eq!(context.get_entity_count::<Animal>(), 0);
+        assert_eq!(context.get_entity_count::<Person>(), 0);
+
+        // Create entities of different kinds
+        for _ in 0..7 {
+            let _: PersonId = context.add_entity((Age(25),)).unwrap();
+        }
+        for _ in 0..5 {
+            let _: AnimalId = context.add_entity((Legs(2),)).unwrap();
+        }
+
+        assert_eq!(context.get_entity_count::<Animal>(), 5);
+        assert_eq!(context.get_entity_count::<Person>(), 7);
+
+        let _: PersonId = context.add_entity((Age(30),)).unwrap();
+        let _: AnimalId = context.add_entity((Legs(8),)).unwrap();
+
+        assert_eq!(context.get_entity_count::<Animal>(), 6);
+        assert_eq!(context.get_entity_count::<Person>(), 8);
+    }
+
+    #[test]
+    fn get_derived_property_multiple_deps() {
+        let mut context = Context::new();
+
+        let expected_high_id: PersonId = context
+            .add_entity((Age(77), Vaccinated(false), InfectionStatus::Susceptible))
+            .unwrap();
+        let expected_med_id: PersonId = context
+            .add_entity((Age(30), Vaccinated(false), InfectionStatus::Susceptible))
+            .unwrap();
+        let expected_low_id: PersonId = context
+            .add_entity((Age(3), Vaccinated(true), InfectionStatus::Recovered))
+            .unwrap();
+
+        let actual_high: RiskLevel = context.get_property(expected_high_id);
+        assert_eq!(actual_high, RiskLevel::High);
+        let actual_med: RiskLevel = context.get_property(expected_med_id);
+        assert_eq!(actual_med, RiskLevel::Medium);
+        let actual_low: RiskLevel = context.get_property(expected_low_id);
+        assert_eq!(actual_low, RiskLevel::Low);
+    }
+
+    #[test]
+    fn listen_to_derived_property_change_event() {
+        let mut context = Context::new();
+
+        let expected_high_id = PersonId::new(0);
+
+        // Listen for derived property change events and record how many times it fires
+        // For `RiskLevel`
+        let risk_flag = Rc::new(RefCell::new(0));
+        let risk_flag_clone = risk_flag.clone();
+        context.subscribe_to_event(
+            move |_context, event: PropertyChangeEvent<Person, RiskLevel>| {
+                assert_eq!(event.entity_id, expected_high_id);
+                assert_eq!(event.previous_value, Some(RiskLevel::High));
+                assert_eq!(event.current_value, RiskLevel::Medium);
+                *risk_flag_clone.borrow_mut() += 1;
+            },
+        );
+        // For `AgeGroup`
+        let age_group_flag = Rc::new(RefCell::new(0));
+        let age_group_flag_clone = age_group_flag.clone();
+        context.subscribe_to_event(
+            move |_context, event: PropertyChangeEvent<Person, AgeGroup>| {
+                assert_eq!(event.entity_id, expected_high_id);
+                assert_eq!(event.previous_value, Some(AgeGroup::Senior));
+                assert_eq!(event.current_value, AgeGroup::Adult);
+                *age_group_flag_clone.borrow_mut() += 1;
+            },
+        );
+
+        // Should not emit change events
+        let expected_high_id: PersonId = context
+            .add_entity((Age(77), Vaccinated(false), InfectionStatus::Susceptible))
+            .unwrap();
+
+        // Should emit change events
+        context.set_property(expected_high_id, Age(20));
+
+        // Execute queued event handlers
+        context.execute();
+        // Should have exactly one event recorded
+        assert_eq!(*risk_flag.borrow(), 1);
+        assert_eq!(*age_group_flag.borrow(), 1);
+    }
+
+    /*
+    ToDo(RobertJacobsonCDC): Enable this once #691 is resolved, https://github.com/CDCgov/ixa/issues/691.
+
+    #[test]
+    fn get_derived_property_with_globals() {
+        let mut context = Context::new();
+
+        context.set_global_property_value(GlobalDummy, 18).unwrap();
+        let child = context.add_entity((Age(17),)).unwrap();
+        let adult = context.add_entity((Age(19),)).unwrap();
+
+        let child_computed: MyDerivedProperty = context.get_property(child);
+        assert_eq!(child_computed, MyDerivedProperty(17+18));
+
+        let adult_computed: MyDerivedProperty = context.get_property(adult);
+        assert_eq!(adult_computed, MyDerivedProperty(19+18));
+    }
+    */
+
+    #[test]
+    fn observe_diamond_property_change() {
+        let mut context = Context::new();
+        let person = context.add_entity((Age(17), IsSwimmer(true))).unwrap();
+
+        let is_adult_athlete: AdultAthlete = context.get_property(person);
+        assert!(!is_adult_athlete.0);
+
+        let flag = Rc::new(RefCell::new(0));
+        let flag_clone = flag.clone();
+        context.subscribe_to_event(
+            move |_context, event: PropertyChangeEvent<Person, AdultAthlete>| {
+                assert_eq!(event.entity_id, person);
+                assert_eq!(event.previous_value, Some(AdultAthlete(false)));
+                assert_eq!(event.current_value, AdultAthlete(true));
+                *flag_clone.borrow_mut() += 1;
+            },
+        );
+
+        context.set_property(person, Age(20));
+        // Make sure the derived property is what we expect.
+        let is_adult_athlete: AdultAthlete = context.get_property(person);
+        assert!(is_adult_athlete.0);
+
+        // Execute queued event handlers
+        context.execute();
+        // Should have exactly one event recorded
+        assert_eq!(*flag.borrow(), 1);
     }
 
     // Tests related to queries and indexing
@@ -562,5 +795,42 @@ mod tests {
         assert_eq!(context.query_entity_count((AgeGroup::Child,)), 0);
         assert_eq!(context.query_entity_count((AgeGroup::Adult,)), 4);
         assert_eq!(context.query_entity_count((AgeGroup::Senior,)), 2);
+    }
+
+    #[test]
+    fn query_unindexed_default_properties() {
+        let mut context = Context::new();
+
+        // Half will have the default value.
+        for idx in 0..10 {
+            if idx % 2 == 0 {
+                context.add_entity((Age(22),)).unwrap();
+            } else {
+                context
+                    .add_entity((Age(22), InfectionStatus::Recovered))
+                    .unwrap();
+            }
+        }
+        // The tail also has the default value
+        for _ in 0..10 {
+            let _: PersonId = context.add_entity((Age(22),)).unwrap();
+        }
+
+        assert_eq!(context.query_entity_count((InfectionStatus::Recovered,)), 5);
+        assert_eq!(
+            context.query_entity_count((InfectionStatus::Susceptible,)),
+            15
+        );
+    }
+
+    #[test]
+    fn query_unindexed_derived_properties() {
+        let mut context = Context::new();
+
+        for _ in 0..10 {
+            let _: PersonId = context.add_entity((Age(22),)).unwrap();
+        }
+
+        assert_eq!(context.query_entity_count((AdultAthlete(false),)), 10);
     }
 }
