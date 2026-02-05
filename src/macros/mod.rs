b428724311f3326e5dc14fe075136eeae427ed36
Robert Jacobson <RJacobson@cdc.gov>
2026-02-05 12:11:06 -0600

    fix: Refactored the `network` module, updated for entities. Fixes #710.
    Updated the integration tests for the refactored `network` module.
    Updated the network-hhmodel example for the refactored `network` module.

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,9 +1,9 @@
 mod all;
 mod assert_almost_eq;
 mod define_data_plugin;
-mod define_edge_type;
 mod define_global_property;
 mod define_report;
 mod define_rng;
+mod edge_impl;
 mod entity_impl;
 mod property_impl;
