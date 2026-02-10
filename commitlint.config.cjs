c0ab16813d61b1ba98be2fffc19e0c064bb190fd
Gil Sharon <as81@cdc.gov>
2026-02-10 10:23:55 -0500

    chore: add mise precommit hook for Conventional Commits

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -33,7 +33,5 @@ module.exports = {
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 100],
-    // Conventional Commits suggests: don't use Sentence/Pascal/Start/UPPER case.
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
   },
 };
