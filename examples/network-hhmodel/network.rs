30e9a3f6b366c473a58b199a074abc845a80d330
Scott Olesen <ulp7@cdc.gov>
2026-04-15 19:09:17 -0400

    Put contacts into network file

--- examples/network-hhmodel/network.rs
+++ examples/network-hhmodel/network.rs
@@ -1,14 +1,18 @@
 use ixa::network::edge::EdgeType;
 use ixa::prelude::*;
 use ixa::{HashSet, HashSetExt};
+use rand_distr::Bernoulli;
 use serde::Deserialize;
 
 use crate::loader::{open_csv, HouseholdId, Id};
+use crate::parameters::Parameters;
 use crate::{Person, PersonId};
 
 define_edge_type!(struct Household, Person);
 define_edge_type!(struct AgeUnder5, Person);
 define_edge_type!(struct Age5to17, Person);
+
+define_rng!(NetworkRng);
 
 #[derive(Deserialize, Debug)]
 struct EdgeRecord {
@@ -56,6 +60,59 @@ fn load_edge_list<ET: EdgeType<Person>>(context: &mut Context, file_name: &str, 
         let p2 = p2_vec[0];
         context.add_edge_bidi(p1, p2, 1.0, inner.clone()).unwrap();
     }
+}
+
+fn sar_to_beta(sar: f64, infectious_period: f64) -> f64 {
+    1.0 - (1.0 - sar).powf(1.0 / infectious_period)
+}
+
+/// Get all the contacts a person will have over a certain duration
+pub fn get_contacts(context: &Context, person_id: PersonId, duration: f64) -> HashSet<PersonId> {
+    let parameters = context
+        .get_global_property_value(Parameters)
+        .unwrap()
+        .clone();
+
+    // Probability of contact during the duration. Note that this assumes that the duration is not too high!
+    let p_hh = duration * sar_to_beta(parameters.sar, parameters.incubation_period);
+    let p_other = duration
+        * sar_to_beta(
+            parameters.sar / parameters.between_hh_transmission_reduction,
+            parameters.incubation_period,
+        );
+
+    let mut infectees = HashSet::new();
+    infectees.extend(
+        get_contacts_by_edge_type::<Household>(context, person_id)
+            .iter()
+            .filter(|_| context.sample_distr(NetworkRng, Bernoulli::new(p_hh).unwrap())),
+    );
+
+    infectees.extend(
+        get_contacts_by_edge_type::<AgeUnder5>(context, person_id)
+            .iter()
+            .filter(|_| context.sample_distr(NetworkRng, Bernoulli::new(p_other).unwrap())),
+    );
+
+    infectees.extend(
+        get_contacts_by_edge_type::<Age5to17>(context, person_id)
+            .iter()
+            .filter(|_| context.sample_distr(NetworkRng, Bernoulli::new(p_other).unwrap())),
+    );
+
+    infectees
+}
+
+/// Get the contacts by edge type
+fn get_contacts_by_edge_type<ET: EdgeType<Person> + 'static>(
+    context: &Context,
+    person_id: PersonId,
+) -> HashSet<PersonId> {
+    context
+        .get_edges::<Person, ET>(person_id)
+        .iter()
+        .map(|e| e.neighbor)
+        .collect()
 }
 
 pub fn init(context: &mut Context, people: &[PersonId]) {
