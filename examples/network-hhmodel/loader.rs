c18edba4a74ca14a3a8353170f3e83c09756ad0a
Scott Olesen <ulp7@cdc.gov>
2026-05-31 17:23:31 -0400

    docs: network-hhmodel example: Separate contact from disease progression (#847)
    * Put contacts into network file
    * Rework example
    * Repair tests
    * docs: Network example: Implement Hash for RR (#864)
    * Use relative rate, not "reduction"
    * Implement Hash for RR, not FloatEq

--- examples/network-hhmodel/loader.rs
+++ examples/network-hhmodel/loader.rs
@@ -5,7 +5,7 @@ use ixa::impl_property;
 use ixa::prelude::*;
 use serde::{Deserialize, Serialize};
 
-use crate::{example_dir, Person, PersonId};
+use crate::{example_dir, Person};
 
 #[derive(Serialize, Deserialize, Copy, Clone, PartialEq, Eq, Debug, Hash)]
 pub struct Id(pub u16);
@@ -39,45 +39,32 @@ struct PeopleRecord {
     household_id: HouseholdId,
 }
 
-fn create_person_from_record(context: &mut Context, record: &PeopleRecord) -> PersonId {
-    context
-        .add_entity(with!(
-            Person,
-            record.id,
-            record.age_group,
-            record.sex,
-            record.household_id
-        ))
-        .unwrap()
-}
-
 pub fn open_csv(file_name: &str) -> Reader<File> {
     let current_dir = example_dir();
     let file_path = current_dir.join(file_name);
     csv::Reader::from_path(file_path).unwrap()
 }
 
-pub fn init(context: &mut Context) -> Vec<PersonId> {
+pub fn init(context: &mut Context) {
     // Load csv and deserialize records
     let mut reader = open_csv("Households.csv");
-    let mut people = Vec::new();
 
     for result in reader.deserialize() {
         let record: PeopleRecord = result.expect("Failed to parse record");
-        people.push(create_person_from_record(context, &record));
+        context
+            .add_entity(with!(
+                Person,
+                record.id,
+                record.age_group,
+                record.sex,
+                record.household_id
+            ))
+            .unwrap();
     }
-
-    context.index_property::<Person, Id>();
-    context.index_property::<Person, HouseholdId>();
-
-    people
 }
 
 #[cfg(test)]
 mod tests {
-    use ixa::context::Context;
-    use ixa::random::ContextRandomExt;
-
     use super::*;
 
     const EXPECTED_ROWS: usize = 1606;
@@ -85,52 +72,52 @@ mod tests {
     #[test]
     fn test_init_expected_rows() {
         let mut context = Context::new();
-        context.init_random(42);
         init(&mut context);
         assert_eq!(context.get_entity_count::<Person>(), EXPECTED_ROWS);
+    }
+
+    // Check there is exactly one matching entity
+    fn assert_exists1(
+        context: &Context,
+        id: Id,
+        age_group: AgeGroup,
+        sex: Sex,
+        hh_id: HouseholdId,
+    ) {
+        assert_eq!(
+            context.query_entity_count(with!(Person, id, age_group, sex, hh_id)),
+            1
+        );
     }
 
     #[test]
     fn test_some_people_load_correctly() {
         let mut context = Context::new();
-        context.init_random(42);
+        init(&mut context);
 
-        let people = init(&mut context);
+        // e.g., the person with data id 676 should be 18-64, female, in household 1
+        assert_exists1(
+            &context,
+            Id(676),
+            AgeGroup::Age18to64,
+            Sex::Female,
+            HouseholdId(1),
+        );
 
-        let person = people[0];
-        assert!(context.match_entity(
-            person,
-            with!(
-                Person,
-                Id(676),
-                AgeGroup::Age18to64,
-                Sex::Female,
-                HouseholdId(1)
-            )
-        ));
+        assert_exists1(
+            &context,
+            Id(213),
+            AgeGroup::AgeUnder5,
+            Sex::Female,
+            HouseholdId(162),
+        );
 
-        let person = people[246];
-        assert!(context.match_entity(
-            person,
-            with!(
-                Person,
-                Id(213),
-                AgeGroup::AgeUnder5,
-                Sex::Female,
-                HouseholdId(162)
-            )
-        ));
-
-        let person = people[1591];
-        assert!(context.match_entity(
-            person,
-            with!(
-                Person,
-                Id(1591),
-                AgeGroup::Age65Plus,
-                Sex::Male,
-                HouseholdId(496)
-            )
-        ));
+        assert_exists1(
+            &context,
+            Id(1591),
+            AgeGroup::Age65Plus,
+            Sex::Male,
+            HouseholdId(496),
+        );
     }
 }
