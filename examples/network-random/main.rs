8bf1c814ee7ded712d5df5bba58d6194fd48ba19
Scott Olesen <ulp7@cdc.gov>
2026-06-25 18:01:44 -0400

    fixup

--- examples/network-random/main.rs
+++ examples/network-random/main.rs
@@ -1,9 +1,5 @@
-use ixa::prelude::*;
 use ixa::runner::run_with_args;
 use ixa_example_network_random::init;
-
-define_entity!(Person);
-define_rng!(MainRng);
 
 fn main() {
     run_with_args(|context, _, _| {
